cef7ce2bb48b7d052165984dbbe7f699ee4102d9
supermeng <supermeng@redfin.tech>
2023-07-12 10:26:28 +0800

    solved buglist 150 ->  我的收藏，点击🌟按钮会复制该影片，并且不会remove该影片from我的收藏list

--- src/components/vod/favoriteVodCard.tsx
+++ src/components/vod/favoriteVodCard.tsx
@@ -1,58 +1,74 @@
-import { StyleSheet, TouchableOpacity, View, Text } from 'react-native';
-import { useTheme } from '@react-navigation/native';
-import { VodType } from '../../types/ajaxTypes';
+import {StyleSheet, TouchableOpacity, View, Text} from 'react-native';
+import {useTheme} from '@react-navigation/native';
+import {VodType} from '../../types/ajaxTypes';
 import VodImageCard from './vodImageCard';
 import FavoriteButton from '../button/favoriteVodButton';
 import VodDescription from './vodDescription';
 
 interface Props {
-    vod: VodType,
-    onPress?: any,
-    params?: any[],
-    btnStyle?: typeof StyleSheet
-    hideFavoriteButton?: boolean
+  vod: VodType;
+  onPress?: any;
+  params?: any[];
+  btnStyle?: typeof StyleSheet;
+  hideFavoriteButton?: boolean;
+  initialFavoriteState: boolean;
 }
-export default function FavoriteVodCard({ vod, onPress, btnStyle, hideFavoriteButton = false, ...params }: Props) {
-    const { colors, spacing, textVariants } = useTheme();
-    return (
-        <View style={{ ...styles.card, gap: spacing.s }}>
-            <VodImageCard vod_img={vod.vod_pic} vodStyle={styles.image} onPress={onPress} />
-            <TouchableOpacity style={{ ...styles.description, gap: spacing.xs }} onPress={onPress} >
-                <View style={{ ...styles.description, gap: spacing.xs }}>
-                    {
-                        hideFavoriteButton
-                            ? <Text numberOfLines={1} style={{
-                                ...textVariants.body,
-                                color: colors.text,
-                                // flex: 1,
-                                flexShrink: 1
-                            }}>
-                                {vod.vod_name}
-                            </Text>
-                            : <FavoriteButton vod={vod} />
-                    }
-                    <VodDescription vod={vod} />
-                </View>
-            </TouchableOpacity>
+export default function FavoriteVodCard({
+  vod,
+  onPress,
+  btnStyle,
+  hideFavoriteButton = false,
+  initialFavoriteState = false,
+  ...params
+}: Props) {
+  const {colors, spacing, textVariants} = useTheme();
+  return (
+    <View style={{...styles.card, gap: spacing.s}}>
+      <VodImageCard
+        vod_img={vod.vod_pic}
+        vodStyle={styles.image}
+        onPress={onPress}
+      />
+      <TouchableOpacity
+        style={{...styles.description, gap: spacing.xs}}
+        onPress={onPress}>
+        <View style={{...styles.description, gap: spacing.xs}}>
+          {hideFavoriteButton ? (
+            <Text
+              numberOfLines={1}
+              style={{
+                ...textVariants.body,
+                color: colors.text,
+                // flex: 1,
+                flexShrink: 1,
+              }}>
+              {vod.vod_name}
+            </Text>
+          ) : (
+            <FavoriteButton vod={vod} initialState={initialFavoriteState} />
+          )}
+          <VodDescription vod={vod} />
         </View>
-    );
+      </TouchableOpacity>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    card: {
-        padding: 10,
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-    },
-    description: {
-        flexGrow: 1,
-        paddingTop: 4,
-        paddingBottom: 8,
-        flexShrink: 1
-    },
-    image: {
-        width: 120,
-        height: 180
-    }
+  card: {
+    padding: 10,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  description: {
+    flexGrow: 1,
+    paddingTop: 4,
+    paddingBottom: 8,
+    flexShrink: 1,
+  },
+  image: {
+    width: 120,
+    height: 180,
+  },
 });
