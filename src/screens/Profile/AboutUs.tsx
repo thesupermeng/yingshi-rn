d6ce1fa1b11f68000ab8bd6904a623846b394e3a
supermeng <supermeng@redfin.tech>
2023-07-21 11:03:40 +0800

    update----> app内的版本信息

--- src/screens/Profile/AboutUs.tsx
+++ src/screens/Profile/AboutUs.tsx
@@ -42,9 +42,7 @@ export default ({navigation}: RootStackScreenProps<'关于我们'>) => {
         <View style={styles.logo}>
           <Logo2 height={icons.sizes.xxl} width={icons.sizes.xxl} />
         </View>
-        <Text style={{textAlign: 'center', ...textVariants.body}}>
-          V1.0.0.1
-        </Text>
+        <Text style={{textAlign: 'center', ...textVariants.body}}>V1.1.0</Text>
         <NotificationModal
           onConfirm={toggleOverlay}
           isVisible={isDialogOpen}
