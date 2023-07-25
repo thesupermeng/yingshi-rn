b2ad0cf545ca1573fba0eaf4d2cfbaa549380fcf
supermeng <supermeng@redfin.tech>
2023-07-25 15:24:06 +0800

    buglist ---> 往下滑动调节亮度和音量的时候，进度条会前后移动，不顺畅

--- src/components/gestures/vod/BrightnessVolumeSlider.tsx
+++ src/components/gestures/vod/BrightnessVolumeSlider.tsx
@@ -1,74 +1,76 @@
-import React, { useEffect } from 'react';
-import { View, StyleSheet, Image, Text } from 'react-native';
+import React, {useEffect} from 'react';
+import {View, StyleSheet, Image, Text} from 'react-native';
 import Moon from '../../../../static/images/Moon.svg';
 import Sun from '../../../../static/images/Sun.svg';
 import MutedVolume from '../../../../static/images/MutedVolume.svg';
 import Volume from '../../../../static/images/Volume.svg';
 
 type Props = {
-    percent: number,
-    icon: any
-}
+  percent: number;
+  icon: any;
+};
 
 export default ({percent, icon}: Props) => {
-
-    return (
-        <View style={styles.smallContainer}>
-            {icon == "Moon" &&
-                <Moon width={24} height={24} style={{ color: 'white' }} />
-            }
-            {icon == "Sun" &&
-                <Sun width={24} height={24} style={{ color: 'white' }} />
-            }
-            {icon == "MutedVolume" &&
-                <MutedVolume width={24} height={24} style={{ color: 'white' }} />
-            }
-            {icon == "Volume" &&
-                <Volume width={24} height={24} style={{ color: 'white' }} />
-            }
-            <View style={styles.statBar}>
-                <View
-                    style={{
-                        height: 5,
-                        borderRadius: 4,
-                        backgroundColor: 'white',
-                        width: `${percent > 100 ? 100 : percent}%`,
-                    }}>
-                </View>
-            </View>
-            <Text style={styles.percentText}>{percent.toFixed()}%</Text>
-        </View>
-    )
-}
+  return (
+    <View style={styles.smallContainer}>
+      {icon == 'Moon' && <Moon width={24} height={24} style={styles.icon} />}
+      {icon == 'Sun' && <Sun width={24} height={24} style={styles.icon} />}
+      {icon == 'MutedVolume' && (
+        <MutedVolume width={24} height={24} style={styles.icon} />
+      )}
+      {icon == 'Volume' && (
+        <Volume width={24} height={24} style={styles.icon} />
+      )}
+      <View style={styles.statBar}>
+        <View
+          style={{
+            height: 5,
+            borderRadius: 4,
+            backgroundColor: 'white',
+            width: `${percent > 100 ? 100 : percent}%`,
+          }}
+        />
+      </View>
+      <Text style={styles.percentText}>{percent.toFixed()}%</Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    smallContainer: {
-        width: '30%',
-        height: 32,
-        borderRadius: 10,
-        backgroundColor: 'rgba(0, 0, 0, 0.6)',
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        position: 'absolute',
-        marginTop: 20,
-        left: '35%',
-        zIndex: 20,
-    },
-    statBar: {
-        width: '70%',
-        height: 5,
-        backgroundColor: 'rgba(74, 153, 233, 0.3)',
-        marginBottom: 'auto',
-        marginTop: 'auto',
-        flexDirection: 'row',
-        borderStyle: 'solid',
-        borderRadius: 4,
-    },
-    percentText: {
-        color: 'white',
-        fontSize: 12,
-        marginLeft: 8,
-    },
+  smallContainer: {
+    width: '35%',
+    height: 32,
+    borderRadius: 10,
+    backgroundColor: 'rgba(0, 0, 0, 0.6)',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    marginTop: 20,
+    left: '35%',
+    zIndex: 20,
+  },
+  statBar: {
+    width: '70%',
+    height: 5,
+    backgroundColor: 'rgba(74, 153, 233, 0.3)',
+    marginBottom: 'auto',
+    marginTop: 'auto',
+    flexDirection: 'row',
+    borderStyle: 'solid',
+    borderRadius: 4,
+  },
+  percentText: {
+    color: 'white',
+    fontSize: 12,
+    width: '15%',
+    marginLeft: 8,
+  },
+  icon: {
+    color: 'white',
+    width: '15%',
+    maxWidth: '15%',
+    objectFix: 'contain',
+  },
 });
--- src/components/videoPlayer/VideoControlsOverlay.tsx
+++ src/components/videoPlayer/VideoControlsOverlay.tsx
@@ -1,142 +1,167 @@
-import React, { useEffect, useState, useContext } from 'react';
-import { View, PanResponder, StyleSheet, Dimensions, Text } from 'react-native';
+import React, {useEffect, useState, useContext} from 'react';
+import {View, PanResponder, StyleSheet, Dimensions, Text} from 'react-native';
 import MiddleControls from './MiddleControls';
 import BottomControls from './BottomControls';
 import TopControls from './TopControls';
 import LinearGradient from 'react-native-linear-gradient';
 import BackButton from '../button/backButton';
-import { useNavigation, useTheme } from '@react-navigation/native';
+import {useNavigation, useTheme} from '@react-navigation/native';
 
 type Props = {
-    currentTime: number,
-    duration: number,
-    onVideoSeek: (params: any) => any,
-    onFastForward: (params: any) => any,
-    onTogglePlayPause: () => any,
-    onHandleFullScreen: () => any,
-    paused: boolean,
-    isFullScreen: boolean,
-    headerTitle: string,
-    onHandleGoBack: () => any,
-    videoType: string
-}
+  currentTime: number;
+  duration: number;
+  onVideoSeek: (params: any) => any;
+  onFastForward: (params: any) => any;
+  onTogglePlayPause: () => any;
+  onHandleFullScreen: () => any;
+  paused: boolean;
+  isFullScreen: boolean;
+  headerTitle: string;
+  onHandleGoBack: () => any;
+  videoType: string;
+};
 
 const height = Dimensions.get('window').width;
 const width = Dimensions.get('window').height;
 
-export default ({ currentTime, duration, onVideoSeek, onFastForward, onTogglePlayPause, onHandleFullScreen, paused, isFullScreen, headerTitle, onHandleGoBack, videoType }: Props) => {
-    const { colors, spacing, textVariants, icons } = useTheme();
-    const navigation = useNavigation();
+export default ({
+  currentTime,
+  duration,
+  onVideoSeek,
+  onFastForward,
+  onTogglePlayPause,
+  onHandleFullScreen,
+  paused,
+  isFullScreen,
+  headerTitle,
+  onHandleGoBack,
+  videoType,
+}: Props) => {
+  const {colors, spacing, textVariants, icons} = useTheme();
+  const navigation = useNavigation();
 
-    const handleFastForward = (time: any) => {
-        onFastForward(time);
-    }
+  const handleFastForward = (time: any) => {
+    onFastForward(time);
+  };
 
-    const handlePlayPause = () => {
-        onTogglePlayPause();
-    }
+  const handlePlayPause = () => {
+    onTogglePlayPause();
+  };
 
-    const onSeek = (time: number) => {
-        // console.log(time);
-        onVideoSeek(time);
-    }
+  const onSeek = (time: number) => {
+    // console.log(time);
+    onVideoSeek(time);
+  };
 
-    const handleFullScreen = () => {
-        onHandleFullScreen();
-    }
-    
-    const goBack = () => {
-        onHandleGoBack();
-    }
+  const handleFullScreen = () => {
+    onHandleFullScreen();
+  };
 
-    const handleToggleEpisodes = () => {
-        
-    }
-    console.log
-    return (
-        <View style={!isFullScreen ? styles.controlsOverlay : [styles.controlsOverlay, { height: height }]}>
-            <View style={styles.videoHeader}>
-                <BackButton btnStyle={{ padding: 20 }} onPress={() => goBack()} />
-                <Text style={{ ...textVariants.body, fontSize: 17, fontWeight: '600', color: colors.text, flex: 1, paddingBottom: 3 }}
-                    numberOfLines={1}>{headerTitle}
-                </Text>
-            </View>
-            {/* Top Controls */}
-            <TopControls
-                isFullScreen={isFullScreen} />
-            {/* Middle Controls */}
-            <MiddleControls
-                fastForward={handleFastForward}
-                togglePlayPause={handlePlayPause}
-                videoType={videoType} 
-                paused={paused} />
-            {/* Bottom Controls */}
-            <BottomControls
-                currentTime={currentTime}
-                duration={duration > 0 ? duration : 0}
-                onSlideStart={handlePlayPause}
-                onSlideComplete={handlePlayPause}
-                onSlideCapture={onSeek}
-                onToggleFullScreen={handleFullScreen}
-                onToggleEpisodes={handleToggleEpisodes}
-                isFullScreen={isFullScreen}
-                videoType={videoType} />
+  const goBack = () => {
+    onHandleGoBack();
+  };
 
-            <LinearGradient
-                colors={['transparent', 'black']}
-                start={{ x: 0.5, y: 0 }}
-                end={{ x: 0.5, y: 0.8 }}
-                style={styles.bottomBlur}
-            />
-            <LinearGradient
-                colors={['transparent', 'black']}
-                start={{ x: 0.5, y: 0.8 }}
-                end={{ x: 0.5, y: 0 }}
-                style={styles.topBlur}
-            />
-        </View>
-    )
-}
+  const handleToggleEpisodes = () => {};
+  console.log;
+  return (
+    <View
+      style={
+        !isFullScreen
+          ? styles.controlsOverlay
+          : [styles.controlsOverlay, {height: height}]
+      }>
+      <View style={styles.videoHeader}>
+        <BackButton btnStyle={{padding: 20}} onPress={() => goBack()} />
+        <Text
+          style={{
+            ...textVariants.body,
+            fontSize: 17,
+            fontWeight: '600',
+            color: colors.text,
+            flex: 1,
+            paddingBottom: 3,
+          }}
+          numberOfLines={1}>
+          {headerTitle}
+        </Text>
+      </View>
+      {/* Top Controls */}
+      <TopControls isFullScreen={isFullScreen} />
+      {/* Middle Controls */}
+      <MiddleControls
+        fastForward={handleFastForward}
+        togglePlayPause={handlePlayPause}
+        videoType={videoType}
+        paused={paused}
+      />
+      {/* Bottom Controls */}
+      <BottomControls
+        currentTime={currentTime}
+        duration={duration > 0 ? duration : 0}
+        onSlideStart={handlePlayPause}
+        onSlideComplete={handlePlayPause}
+        onSlideCapture={onSeek}
+        onToggleFullScreen={handleFullScreen}
+        onToggleEpisodes={handleToggleEpisodes}
+        isFullScreen={isFullScreen}
+        videoType={videoType}
+      />
+
+      <LinearGradient
+        colors={['transparent', 'black']}
+        start={{x: 0.5, y: 0}}
+        end={{x: 0.5, y: 0.8}}
+        style={styles.bottomBlur}
+      />
+      <LinearGradient
+        colors={['transparent', 'black']}
+        start={{x: 0.5, y: 0.8}}
+        end={{x: 0.5, y: 0}}
+        style={styles.topBlur}
+      />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    controlsOverlay: {
-        position: 'absolute',
-        top: 0,
-        bottom: 0,
-        left: 0,
-        right: 0,
-        // backgroundColor: '#00000090',
-        backgroundColor: '#00000010',
-        justifyContent: 'space-between',
-    },
-    fullScreenBottom: {
-        paddingBottom: 60
-    },
-    bottomBlur: {
-        position: 'absolute',
-        left: 0,
-        right: 0,
-        bottom: 0,
-        height: 80,
-        flex: 1,
-        opacity: 0.8
-    },
-    topBlur: {
-        position: 'absolute',
-        left: 0,
-        right: 0,
-        top: 0,
-        height: 100,
-        flex: 1,
-        opacity: 0.8
-    },
-    videoHeader: {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'center',
-        zIndex: 50,
-    },
+  controlsOverlay: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    // backgroundColor: '#00000090',
+    backgroundColor: '#00000010',
+    justifyContent: 'space-between',
+  },
+  fullScreenBottom: {
+    paddingBottom: 60,
+  },
+  bottomBlur: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    bottom: 0,
+    height: 80,
+    flex: 1,
+    opacity: 0.8,
+  },
+  topBlur: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    height: 100,
+    flex: 1,
+    opacity: 0.8,
+  },
+  videoHeader: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    zIndex: 50,
+  },
 });
