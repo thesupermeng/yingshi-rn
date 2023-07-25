bd02cc5ae5949ad8964b49ebe469cda500b0b927
supermeng <supermeng@redfin.tech>
2023-07-25 10:05:47 +0800

    slve bugslist ---------->. 播放条的白色按钮太大，并且播放器里的按钮点了有时候没有反应

--- src/components/videoPlayer/MiddleControls.tsx
+++ src/components/videoPlayer/MiddleControls.tsx
@@ -1,66 +1,63 @@
-import React, { useEffect, useState, useContext } from 'react';
-import { View, PanResponder, StyleSheet, TouchableOpacity } from 'react-native';
+import React, {useEffect, useState, useContext} from 'react';
+import {View, PanResponder, StyleSheet, TouchableOpacity} from 'react-native';
 import Play from '../../../static/images/blackPlay.svg';
 import Pause from '../../../static/images/pause.svg';
 import Rewind from '../../../static/images/rewind.svg';
 import Fastforward from '../../../static/images/fastforward.svg';
 
 type Props = {
-    fastForward: (params: any) => any,
-    togglePlayPause: () => any,
-    paused: boolean
-    videoType?: string
-}
+  fastForward: (params: any) => any;
+  togglePlayPause: () => any;
+  paused: boolean;
+  videoType?: string;
+};
 
-export default ({ fastForward, togglePlayPause, paused, videoType }: Props) => {
+export default ({fastForward, togglePlayPause, paused, videoType}: Props) => {
+  useEffect(() => {}, []);
 
-    useEffect(() => {
+  const onSkip = (seconds: number) => {
+    fastForward(seconds);
+  };
 
-    }, [])
+  const onTogglePlayPause = () => {
+    togglePlayPause();
+  };
 
-    const onSkip = (seconds: number) => {
-        fastForward(seconds);
-    }
-
-    const onTogglePlayPause = () => {
-        togglePlayPause();
-    }
-
-    return (
-        <View style={styles.middleControls}>
-            {
-                videoType !== 'live' &&
-                <TouchableOpacity style={styles.sideButtons} onPress={() => onSkip(-10)}>
-                    <Rewind width={60} height={60} />
-                </TouchableOpacity>
-            }
-            <TouchableOpacity onPress={() => onTogglePlayPause()}>
-                {paused ?
-                    <Play width={55} height={55} />
-                    :
-                    <Pause width={55} height={55} />
-                }
-            </TouchableOpacity>
-            {
-                videoType !== 'live' &&
-                <TouchableOpacity style={styles.sideButtons} onPress={() => onSkip(10)}>
-                    <Fastforward width={60} height={60} />
-                </TouchableOpacity>
-            }
-        </View>
-    )
-}
+  return (
+    <View style={styles.middleControls}>
+      {videoType !== 'live' && (
+        <TouchableOpacity
+          style={styles.sideButtons}
+          onPress={() => onSkip(-10)}>
+          <Rewind width={68} height={68} />
+        </TouchableOpacity>
+      )}
+      <TouchableOpacity onPress={() => onTogglePlayPause()}>
+        {paused ? (
+          <Play width={55} height={55} />
+        ) : (
+          <Pause width={55} height={55} />
+        )}
+      </TouchableOpacity>
+      {videoType !== 'live' && (
+        <TouchableOpacity style={styles.sideButtons} onPress={() => onSkip(10)}>
+          <Fastforward width={68} height={68} />
+        </TouchableOpacity>
+      )}
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    middleControls: {
-        paddingHorizontal: 5,
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-        flex: 3,
-    },
-    sideButtons: {
-        paddingLeft: 20,
-        paddingRight: 20,
-    }
+  middleControls: {
+    paddingHorizontal: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 3,
+  },
+  sideButtons: {
+    paddingLeft: 46,
+    paddingRight: 46,
+  },
 });
