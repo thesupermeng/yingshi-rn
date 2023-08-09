de8d14e6ddc6f01765f9619d4e686e6ca0db3e77
supermeng <supermeng@redfin.tech>
2023-08-09 10:34:25 +0800

    add missing component

---
+++ src/components/common/noConnection.tsx
@@ -0,0 +1,89 @@
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextStyle,
+  TouchableOpacity,
+} from 'react-native';
+import {useTheme} from '@react-navigation/native';
+import NoWifi from '../../../static/images/no-wifi.svg';
+import RefreshIcon from '../../../static/images/refresh.svg';
+interface Props {
+  onClickRetry?: any;
+}
+export default function NoConnection({onClickRetry}: Props) {
+  const {textVariants, colors, icons} = useTheme();
+  return (
+    <View style={{...styles.container}}>
+      <NoWifi />
+      <Text
+        style={{
+          ...textVariants.subBody,
+          color: colors.text,
+          textAlign: 'center',
+          paddingTop: 10,
+        }}>
+        无网络
+      </Text>
+      <Text
+        style={{
+          ...textVariants.subBody,
+          color: colors.muted,
+          textAlign: 'center',
+          paddingTop: 12,
+        }}>
+        网络连接失败，请刷新重试
+      </Text>
+
+      <TouchableOpacity activeOpacity={0.7} onPress={onClickRetry}>
+        <View
+          style={{
+            ...styles.refreshBtn,
+            backgroundColor: colors.title,
+          }}>
+          <Text
+            style={{
+              ...styles.head,
+              color: colors.background,
+            }}>
+            <RefreshIcon /> 点击重试
+          </Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexGrow: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingBottom: '50%',
+  },
+  btns: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  btn: {
+    flex: 1,
+  },
+  head: {
+    color: 'white',
+    textAlign: 'center',
+  },
+  refreshBtn: {
+    marginTop: 20,
+    borderRadius: 30,
+    display: 'flex',
+    flexDirection: 'row',
+    paddingVertical: 8,
+    paddingHorizontal: 15,
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontWeight: 'bold',
+  },
+});
