887e02a381ae4b5aa69b6f6595b436413484e450
supermeng <supermeng@redfin.tech>
2023-08-30 11:01:32 +0800

    added logout function

--- src/screens/Profile/Configure.tsx
+++ src/screens/Profile/Configure.tsx
@@ -8,7 +8,7 @@ import {
 } from 'react-native';
 import ScreenContainer from '../../components/container/screenContainer';
 import {RootStackScreenProps} from '../../types/navigationTypes';
-import {useTheme} from '@react-navigation/native';
+import {useNavigation, useTheme} from '@react-navigation/native';
 
 import TitleWithBackButtonHeader from '../../components/header/titleWithBackButtonHeader';
 import {Button, Dialog} from '@rneui/themed';
@@ -19,11 +19,21 @@ import ConfirmationModal from '../../components/modal/confirmationModal';
 import {useAppDispatch} from '../../hooks/hooks';
 import {clearStorageMemory} from '../../redux/actions/settingsActions';
 import NetInfo, {NetInfoState} from '@react-native-community/netinfo';
+
+import {removeUserAuthState} from '../../redux/actions/userAction';
+
 export default ({navigation}: RootStackScreenProps<'设置'>) => {
   const {colors, textVariants, icons, spacing} = useTheme();
   const [isVersionDialogOpen, setIsVersionDialogOpen] = useState(false);
   const [isClearDialogOpen, setIsClearDialogOpen] = useState(false);
+
+  const [isLogoutDialogOpen, setIsLogoutDialogOpen] = useState(false);
+
   const dispatch = useAppDispatch();
+
+  const toggleLogoutDialog = () => {
+    setIsLogoutDialogOpen(!isLogoutDialogOpen);
+  };
 
   const toggleVersionDialog = () => {
     setIsVersionDialogOpen(!isVersionDialogOpen);
@@ -31,7 +41,7 @@ export default ({navigation}: RootStackScreenProps<'设置'>) => {
   const toggleClearDialog = () => {
     setIsClearDialogOpen(!isClearDialogOpen);
   };
-
+  const navigator = useNavigation();
   const [isOffline, setIsOffline] = useState(false);
   useEffect(() => {
     const removeNetInfoSubscription = NetInfo.addEventListener(
@@ -45,57 +55,96 @@ export default ({navigation}: RootStackScreenProps<'设置'>) => {
   }, []);
   return (
     <ScreenContainer>
-      <View style={{gap: spacing.m}}>
-        <TitleWithBackButtonHeader title="设置" />
-
-        <NotificationModal
-          onConfirm={toggleVersionDialog}
-          isVisible={isVersionDialogOpen && !isOffline}
-          title="检查更新"
-          subtitle1="当前已是最新版本1.3.0"
-          confirmationText="我知道了"
-        />
-
-        <NotificationModal
-          isVisible={isVersionDialogOpen && isOffline}
-          onConfirm={toggleVersionDialog}
-          title="无法检测网络，请稍后再试"
-        />
-
-        <ConfirmationModal
-          onConfirm={() => {
-            dispatch(clearStorageMemory());
-            toggleClearDialog();
-          }}
-          onCancel={toggleClearDialog}
-          isVisible={isClearDialogOpen}
-          title="空间清理"
-          subtitle="清除缓存不会影响播放历史和我的收藏"
-          confirmationText="清除"
-        />
+      <View style={{gap: spacing.m, justifyContent: 'space-between', flex: 1}}>
         <View>
-          <ShowMoreButton text="空间清理" onPress={toggleClearDialog} />
-          <ShowMoreButton
-            text="检查更新"
-            onPress={toggleVersionDialog}
-            rightIcon={
-              <View style={styles.icon}>
-                <Text
-                  style={{
-                    ...textVariants.small,
-                    paddingBottom: 3,
-                    color: colors.muted,
-                  }}>
-                  当前版本1.3.0
-                </Text>
-                <MoreArrow
-                  width={icons.sizes.l}
-                  height={icons.sizes.l}
-                  color={colors.muted}
-                />
-              </View>
-            }
+          <TitleWithBackButtonHeader title="设置" />
+
+          <NotificationModal
+            onConfirm={toggleVersionDialog}
+            isVisible={isVersionDialogOpen && !isOffline}
+            title="检查更新"
+            subtitle1="当前已是最新版本1.3.0"
+            confirmationText="我知道了"
           />
+
+          <NotificationModal
+            isVisible={isVersionDialogOpen && isOffline}
+            onConfirm={toggleVersionDialog}
+            title="无法检测网络，请稍后再试"
+          />
+
+          <ConfirmationModal
+            onConfirm={() => {
+              dispatch(clearStorageMemory());
+              toggleClearDialog();
+            }}
+            onCancel={toggleClearDialog}
+            isVisible={isClearDialogOpen}
+            title="空间清理"
+            subtitle="清除缓存不会影响播放历史和我的收藏"
+            confirmationText="清除"
+          />
+
+          <ConfirmationModal
+            onConfirm={async () => {
+              //    user logout
+              await dispatch(removeUserAuthState());
+              navigator.navigate('Home', {
+                screen: 'Profile',
+                showSuccessRegister: true,
+              });
+              toggleLogoutDialog();
+            }}
+            onCancel={toggleLogoutDialog}
+            isVisible={isLogoutDialogOpen}
+            title="退出登录"
+            subtitle="您是否确定要退出登录？"
+            confirmationText="确定"
+          />
+
+          {/* displayed content */}
+          <View>
+            <View>
+              <ShowMoreButton text="空间清理" onPress={toggleClearDialog} />
+              <ShowMoreButton
+                text="检查更新"
+                onPress={toggleVersionDialog}
+                rightIcon={
+                  <View style={styles.icon}>
+                    <Text
+                      style={{
+                        ...textVariants.small,
+                        paddingBottom: 3,
+                        color: colors.muted,
+                      }}>
+                      当前版本1.3.0
+                    </Text>
+                    <MoreArrow
+                      width={icons.sizes.l}
+                      height={icons.sizes.l}
+                      color={colors.muted}
+                    />
+                  </View>
+                }
+              />
+            </View>
+          </View>
+        </View>
+
+        <View
+          style={{
+            backgroundColor: '#1d2023',
+            width: '100%',
+            height: 50,
+            borderRadius: 8,
+            borderWidth: 0,
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginBottom: 30,
+          }}>
+          <TouchableOpacity onPress={toggleLogoutDialog}>
+            <Text style={{color: '#FF3C3C'}}>退出登陆</Text>
+          </TouchableOpacity>
         </View>
       </View>
     </ScreenContainer>
