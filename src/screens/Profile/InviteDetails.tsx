7b0f52bbae40224971633ebef3f72e9063877e4a
supermeng <supermeng@redfin.tech>
2023-09-13 10:26:31 +0800

    cater new API format

--- src/screens/Profile/InviteDetails.tsx
+++ src/screens/Profile/InviteDetails.tsx
@@ -87,6 +87,7 @@ export default ({navigation}: RootStackScreenProps<'邀请详情'>) => {
       let displayText = '';
       if (item.text) {
         displayText = item.text;
+        item.invited_vip_reward_day = item.vip_reward_day;
       } else {
         displayText = item.user_name + '接受了您的邀请';
       }
