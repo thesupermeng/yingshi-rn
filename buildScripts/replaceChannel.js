b9d62042d7a065fd9f9446c4a6ab189abed0efbe
juicy <juicy@redfin.tech>
2023-10-09 15:09:58 +0800

    Modified bundling script

--- buildScripts/replaceChannel.js
+++ buildScripts/replaceChannel.js
@@ -2,8 +2,27 @@ const replace = require('replace-in-file');
 const fs = require('fs');
 
 const constantsFilePath = '../src/utility/constants.ts';
+const androidKeyFile = '../android/app/src/main/res/values/strings.xml';
+const package = require('../package.json');
+const versionNum = package.version;
+const packageName = package.name;
 
-let versionNum = "1.3.0";
+let codePushDeploymentKey = "";
+let umengKey = "";
+
+if (packageName == "yingshipin") {
+    // yingshipin Staging Key
+    // codePushDeploymentKey = "o-gu4vgW0ne79pMzU8HNVjZzJjTcBFUnXqx1I"
+    // yingshipin Production Key
+    codePushDeploymentKey = "SVDKNhmyEZXHTxX14U0uxQwSOAwc9me-xnj7U"
+    umengKey = "650938dcb2f6fa00ba556645"
+} else {
+    // yingshi Staging Key
+    // codePushDeploymentKey = "TJMfhAG-GnqKD6ygv0iOCXPvwwraf7MT-89Tm"
+    // yingshi Production Key
+    codePushDeploymentKey = "6AU3ZnXjdVagR29vAWza6a0rNeXC_372DupHn"
+    umengKey = "648ec50987568a379b587f39"
+}
 
 fs.readFile(constantsFilePath, 'utf8', (err, data) => {
     if (err) {
@@ -18,14 +37,25 @@ fs.readFile(constantsFilePath, 'utf8', (err, data) => {
     }
 });
 
-const ALL_UMENG_CHANNELS = {
-    'WEB': 'WEB',
-    'GOOGLE_PLAY': 'GOOGLE_PLAY',
-    'HUAWEI_APP_GALLERY_OVERSEAS': 'HUAWEI_APP_GALLERY_OVERSEAS',
-    'OPPO_APP_MARKET': 'OPPO_APP_MARKET',
-    'VIVO_APP_STORE': 'VIVO_APP_STORE',
-    'MI_GETAPPS': 'MI_GETAPPS'
-}
+fs.readFile(androidKeyFile, 'utf8', (err, data) => {
+    if (err) {
+        console.error('Error reading the file:', err);
+        return;
+    }
+
+    const updatedCodePush = data.replace(
+        /<string\s+moduleConfig="true"\s+name="CodePushDeploymentKey">[^<]+<\/string>/,
+        '<string moduleConfig="true" name="CodePushDeploymentKey">' + codePushDeploymentKey + '</string>'
+    );
+
+    fs.writeFile(androidKeyFile, updatedCodePush, 'utf8', (err) => {
+        if (err) {
+            console.error('Error writing to the file:', err);
+            return;
+        }
+        console.log('File updated successfully.');
+    });
+});
 
 const channel = process.env.APK_CHANNEL;
 console.log(process.env.APK_CHANNEL);
@@ -42,8 +72,15 @@ const replaceToChannelAndroid = {
     to: 'resValue "string", "UMENG_CHANNEL", "DEFAULT"',
 };
 
+const replaceToUmengAppKey = {
+    files: ['../android/app/build.gradle'],
+    from: /resValue "string", "UMENG_APPKEY", "([^"]+)"/g,
+    to: 'resValue "string", "UMENG_APPKEY", "648ec50987568a379b587f39"',
+};
+
 replaceToChannelRN.to = 'export const UMENG_CHANNEL = "' + channel + '"';
 replaceToChannelAndroid.to = 'resValue "string", "UMENG_CHANNEL", "' + channel + '"';
+replaceToUmengAppKey.to = 'resValue "string", "UMENG_APPKEY", "' + umengKey + '"';
 
 console.log(replaceToChannelRN.to);
 
@@ -66,6 +103,15 @@ try {
 } catch (error) {
     console.error('Error occurred:', error);
 }
+
+// const ALL_UMENG_CHANNELS = {
+//     'WEB': 'WEB',
+//     'GOOGLE_PLAY': 'GOOGLE_PLAY',
+//     'HUAWEI_APP_GALLERY_OVERSEAS': 'HUAWEI_APP_GALLERY_OVERSEAS',
+//     'OPPO_APP_MARKET': 'OPPO_APP_MARKET',
+//     'VIVO_APP_STORE': 'VIVO_APP_STORE',
+//     'MI_GETAPPS': 'MI_GETAPPS'
+// }
 
 // const channels = Object.entries(ALL_UMENG_CHANNELS);
 // channels.forEach(([key, value]) => {
