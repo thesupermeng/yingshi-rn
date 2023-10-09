8322ba1ca26b8d82e6ec2dccc567d2d8cbb73f7f
juicy <juicy@redfin.tech>
2023-10-09 17:36:54 +0800

    TOpon ads placementid

--- android/app/src/main/java/com/yingshipin/SplashAdShowActivity.java
+++ android/app/src/main/java/com/yingshipin/SplashAdShowActivity.java
@@ -97,14 +97,14 @@ public class SplashAdShowActivity extends Activity implements ATSplashExListener
         //Klevin
 //        defaultConfig = "{\"unit_id\":1333253,\"nw_firm_id\":51,\"adapter_class\":\"com.anythink.network.klevin.KlevinATSplashAdapter\",\"content\":\"{\\\"pos_id\\\":\\\"30029\\\",\\\"app_id\\\":\\\"30008\\\"}\"}";
 
-        splashAd = new ATSplashAd(this, "b65002d6fd72be", this, 10000, defaultConfig);
+        splashAd = new ATSplashAd(this, "b650d315a31bc4", this, 10000, defaultConfig);
 
         Map<String, Object> localMap = new HashMap<>();
         localMap.put(ATAdConst.KEY.AD_WIDTH, layoutParams.width);
         localMap.put(ATAdConst.KEY.AD_HEIGHT, layoutParams.height);
 
         splashAd.setLocalExtra(localMap);
-        ATSplashAd.entryAdScenario("b65002d6fd72be", "f628c7999265cd");
+        ATSplashAd.entryAdScenario("b650d315a31bc4", "f628c7999265cd");
 
         splashAd.setAdSourceStatusListener(new ATAdSourceStatusListener() {
             @Override
