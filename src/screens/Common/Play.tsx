3b08bfc7b738e29e5b0581ef911834d8cc7f6597
elena <elena@redfin.tech>
2023-10-13 09:37:33 +0800

    fix: currentTimeRef not updated when vod changed

--- src/screens/Common/Play.tsx
+++ src/screens/Common/Play.tsx
@@ -158,6 +158,7 @@ const insets = useSafeAreaInsets();
   useEffect(() => {
     if (vod) {
       setInitTime(vod?.timeWatched)
+      currentTimeRef.current = vod.timeWatched;
     }
   }, [vod])
 
