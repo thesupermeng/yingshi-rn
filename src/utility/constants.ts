4b21af46947d4ac6403e98ee783b45846d33b278
jianhui <jianhui@redfin.tech>
2023-10-17 11:10:03 +0800

    add platform constants

--- src/utility/constants.ts
+++ src/utility/constants.ts
@@ -80,3 +80,9 @@ export const ANDROID_HOME_REWARD_ADS = 'b651e77ae56eda';
 export const TOPON_BANNER_WIDTH = 415;
 export const TOPON_BANNER_HEIGHT = 50;
 export const TOPON_BANNER_RATIO = TOPON_BANNER_HEIGHT / TOPON_BANNER_WIDTH;
+
+// app platform
+export const YING_SHI_PIN_ANDROID = 1;
+export const YING_SHI_PIN_IOS = 2;
+export const YING_SHI_TV_ANDROID = 3;
+export const YING_SHI_TV_IOS = 4;
