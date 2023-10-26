73134f8408c4de353f3c62263f257f517704ae1e
kelvin <kelvin@redfin.tech>
2023-10-26 08:55:08 +0800

    bug fix video player will jump up when on init play screen

--- src/screens/Common/Play.tsx
+++ src/screens/Common/Play.tsx
@@ -138,13 +138,10 @@ export default ({ navigation, route }: RootStackScreenProps<"播放">) => {
   const onShare = async () => {
     try {
       const result = await Share.share({
-        message: `《${
-          vod?.vod_name
-        }》高清播放${"\n"}https://yingshi.tv/index.php/vod/play/id/${
-          vod?.vod_id
-        }/sid/1/nid/${
-          currentEpisode + 1
-        }.html${"\n"}${APP_NAME_CONST}-海量高清视频在线观看`,
+        message: `《${vod?.vod_name
+          }》高清播放${"\n"}https://yingshi.tv/index.php/vod/play/id/${vod?.vod_id
+          }/sid/1/nid/${currentEpisode + 1
+          }.html${"\n"}${APP_NAME_CONST}-海量高清视频在线观看`,
       });
       if (result.action === Share.sharedAction) {
         if (result.activityType) {
@@ -205,10 +202,10 @@ export default ({ navigation, route }: RootStackScreenProps<"播放">) => {
   const fetchVodDetails = () =>
     fetch(
       `${API_DOMAIN}vod/v1/vod/detail?id=${vod?.vod_id}&appName=${APP_NAME_CONST}&platform=` +
-        Platform.OS.toUpperCase() +
-        `&channelId=` +
-        UMENG_CHANNEL +
-        `&ip=${localIp}`
+      Platform.OS.toUpperCase() +
+      `&channelId=` +
+      UMENG_CHANNEL +
+      `&ip=${localIp}`
     )
       .then((response) => response.json())
       .then((json: VodDetailsResponseType) => {
@@ -366,37 +363,35 @@ export default ({ navigation, route }: RootStackScreenProps<"播放">) => {
         {isVodRestricted && vod && !isOffline && <BingSearch vod={vod} />}
 
         {!isVodRestricted &&
-          vod?.vod_play_list?.urls?.find((url) => url.nid === currentEpisode)
-            ?.url !== undefined &&
           !dismountPlayer &&
           !isOffline && (
             <VodPlayer
               vod_url={
-                vod.vod_play_list.urls.find((url) => url.nid === currentEpisode)
+                vod?.vod_play_list.urls?.find((url) => url.nid === currentEpisode)
                   ?.url
               }
               ref={videoPlayerRef}
               currentTimeRef={currentTimeRef}
               initialStartTime={initTime}
-              vodTitle={vod.vod_name}
+              vodTitle={vod?.vod_name}
               videoType="vod"
               activeEpisode={currentEpisode}
-              episodes={vod.type_id !== 2 ? vod?.vod_play_list : undefined}
+              episodes={vod?.type_id !== 2 ? vod?.vod_play_list : undefined}
               onEpisodeChange={(id: number) => {
                 setCurrentEpisode(id);
                 currentTimeRef.current = 0;
               }}
               showGuide={settingsReducer.showVodPlayerGuide}
               rangeSize={EPISODE_RANGE_SIZE}
-              autoPlayNext={vod.type_id !== 2}
+              autoPlayNext={vod?.type_id !== 2}
               onShare={onShare}
-              movieList={vod.type_id === 2 ? suggestedVods : []}
-              showMoreType={vod.type_id === 2 ? "movies" : "episodes"}
+              movieList={vod?.type_id === 2 ? suggestedVods : []}
+              showMoreType={vod?.type_id === 2 ? "movies" : "episodes"}
               isFetchingRecommendedMovies={isFetchingSuggestedVod}
               appOrientation={settingsReducer.appOrientation}
               devicesOrientation={settingsReducer.devicesOrientation}
               lockOrientation={lockOrientation}
-              // setNavBarOptions={setNavBarOptions}
+            // setNavBarOptions={setNavBarOptions}
             />
           )}
         {isOffline && dismountPlayer && (
@@ -497,15 +492,14 @@ export default ({ navigation, route }: RootStackScreenProps<"播放">) => {
                     <Text
                       style={{ ...textVariants.subBody, color: colors.muted }}
                     >
-                      {`更新：${
-                        vod
-                          ? new Date(vod?.vod_time_add * 1000)
-                              .toLocaleDateString("en-GB")
-                              .replace(/\//g, "-")
-                          : new Date()
-                              .toLocaleDateString("en-GB")
-                              .replace(/\//g, "-")
-                      }`}
+                      {`更新：${vod
+                        ? new Date(vod?.vod_time_add * 1000)
+                          .toLocaleDateString("en-GB")
+                          .replace(/\//g, "-")
+                        : new Date()
+                          .toLocaleDateString("en-GB")
+                          .replace(/\//g, "-")
+                        }`}
                     </Text>
                     <TouchableOpacity onPress={onShare}>
                       <View style={{ ...styles.share, gap: 10 }}>
@@ -614,9 +608,8 @@ export default ({ navigation, route }: RootStackScreenProps<"播放">) => {
                                     color: colors.muted,
                                     fontSize: 15,
                                   }}
-                                >{`${
-                                  showEpisodeRangeStart + 1
-                                }-${showEpisodeRangeEnd}集`}</Text>
+                                >{`${showEpisodeRangeStart + 1
+                                  }-${showEpisodeRangeEnd}集`}</Text>
                                 <MoreArrow
                                   style={{ color: colors.muted }}
                                   height={icons.sizes.m}
@@ -628,7 +621,7 @@ export default ({ navigation, route }: RootStackScreenProps<"播放">) => {
                               horizontal={true}
                               showsHorizontalScrollIndicator={false}
                               initialNumToRender={10}
-                              onScrollToIndexFailed={() => {}}
+                              onScrollToIndexFailed={() => { }}
                               ref={episodeRef}
                               data={vod?.vod_play_list.urls.slice(
                                 showEpisodeRangeStart,
