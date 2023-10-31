721aae4cfe66a492174b94a0aca5f328d015ee24
supermeng <supermeng@redfin.tech>
2023-10-31 17:37:51 +0800

    use const for user and feedback

--- src/utility/constants.ts
+++ src/utility/constants.ts
@@ -86,10 +86,13 @@ export const TOPON_BANNER_HEIGHT = 50;
 export const TOPON_BANNER_RATIO = TOPON_BANNER_HEIGHT / TOPON_BANNER_WIDTH;
 
 // app platform
-export const YING_SHI_PIN_ANDROID = 1;
-export const YING_SHI_PIN_IOS = 2;
-export const YING_SHI_TV_ANDROID = 3;
-export const YING_SHI_TV_IOS = 4;
+export const YING_SHI_PRODUCT_ANDROID = 1;
+export const YING_SHI_PRODUCT_IOS = 2;
+
+// export const YING_SHI_PIN_ANDROID = 1;
+// export const YING_SHI_PIN_IOS = 2;
+// export const YING_SHI_TV_ANDROID = 3;
+// export const YING_SHI_TV_IOS = 4;
 
 export const APP_NAME_CONST = '萤视频';
 export const APP_EMAIL_CONST = 'yingshipin.contactus@gmail.com';
