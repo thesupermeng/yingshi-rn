68ed75d837e6530b901dad529c7c195566b603a9
supermeng <supermeng@redfin.tech>
2023-11-02 15:46:55 +0800

    solve login success image not showing

--- src/navigation/nav.tsx
+++ src/navigation/nav.tsx
@@ -68,7 +68,7 @@ import {
   SafeAreaProvider,
   useSafeAreaInsets,
 } from "react-native-safe-area-context";
-import { Image, StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import DeviceInfo from "react-native-device-info";
 import { useAppSelector, useAppDispatch } from "../hooks/hooks";
 import { QueryClient, useQuery } from "@tanstack/react-query";
@@ -529,7 +529,8 @@ export default () => {
         backdropStyle={{ backgroundColor: "rgba(0, 0, 0, 0.8)" }}
         onBackdropPress={() => setIsDialogOpen(false)}
       >
-        <Image
+        <FastImage
+          useFastImage={true}
           key={gifKey}
           style={{
             height: 80,
@@ -540,7 +541,7 @@ export default () => {
             justifyContent: "center",
             alignItems: "center",
           }}
-          resizeMode="contain"
+          resizeMode={"contain"}
           source={require("../../static/images/profile/login-success.gif")}
         />
 
