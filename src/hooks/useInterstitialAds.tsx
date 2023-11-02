8307d0c0e970abea678fa5f2638366d285c21e94
supermeng <supermeng@redfin.tech>
2023-11-02 16:56:26 +0800

    ads fix

--- src/hooks/useInterstitialAds.tsx
+++ src/hooks/useInterstitialAds.tsx
@@ -15,20 +15,25 @@ import { RootState } from "../redux/store";
 import { useAppSelector } from "./hooks";
 import { AdsBannerContext } from "../contexts/AdsBannerContext";
 
-type PlacementId = typeof ANDROID_HOME_PAGE_POP_UP_ADS | typeof IOS_HOME_PAGE_POP_UP_ADS;
+type PlacementId =
+  | typeof ANDROID_HOME_PAGE_POP_UP_ADS
+  | typeof IOS_HOME_PAGE_POP_UP_ADS;
 
-const useInterstitialAds = () =>{
-  const [adsReadyFlag, setAdsReadyFlag]= useState(false)
+const useInterstitialAds = () => {
+  const [adsReadyFlag, setAdsReadyFlag] = useState(false);
   const userState: userModel = useAppSelector(
     ({ userReducer }: RootState) => userReducer
   );
-  const {currentRoute} = useContext(AdsBannerContext);
-  const [visitCount, setVisitCount] = useState<Record<string, number>>({})
+  const { currentRoute } = useContext(AdsBannerContext);
+  const [visitCount, setVisitCount] = useState<Record<string, number>>({});
+
+  let isAdsShown = false;
+  let reTryLoad = 0;
 
   ATInterstitialRNSDK.setAdListener(
     ATInterstitialRNSDK.onInterstitialLoaded,
-    (event:any) => {
-      setAdsReadyFlag(true)
+    (event: any) => {
+      setAdsReadyFlag(true);
     }
   );
 
@@ -39,75 +44,136 @@ const useInterstitialAds = () =>{
     //    settings[ATInterstitialRNSDK.UseRewardedVideoAsInterstitial] = true;
 
     ATInterstitialRNSDK.loadAd(interstitialPlacementId, settings);
-
   };
 
   const isInterstitialReady = async (interstitialPlacementId: PlacementId) => {
-    const ready = await ATInterstitialRNSDK.hasAdReady(interstitialPlacementId)
-    setAdsReadyFlag(ready)
+    const ready = await ATInterstitialRNSDK.hasAdReady(interstitialPlacementId);
+    setAdsReadyFlag(ready);
+    if (ready) {
+      isAdsShown = true;
+      ATInterstitialRNSDK.showAd(interstitialPlacementId);
+    } else {
+      setTimeout(() => {
+        showInterstitial(interstitialPlacementId);
+      }, 500);
+    }
   };
 
   const showInterstitial = async (interstitialPlacementId: PlacementId) => {
-    if (adsReadyFlag){
-      ATInterstitialRNSDK.showAd(interstitialPlacementId);
+    if (reTryLoad > 5 || isAdsShown == true) {
+      return;
+    }
+
+    // not vip
+    if (
+      Number(userState.userMemberExpired) <=
+        Number(userState.userCurrentTimestamp) ||
+      userState.userToken === ""
+    ) {
+      loadInterstitial(interstitialPlacementId);
+      setTimeout(() => {
+        isInterstitialReady(interstitialPlacementId);
+      }, 500);
     } else {
-      console.log('interstitial not ready bruh')
+      console.log("VIP no ads");
     }
   };
 
-  const prepareInterstitial = async (interstitialPlacementId: PlacementId) => {
-    for (let i=0; i<1; i++){
-      await isInterstitialReady(interstitialPlacementId)
-      console.log('try count', i)
-      if (i > 5 || adsReadyFlag){
-        break;
-      }
-      await loadInterstitial(interstitialPlacementId)
-      await new Promise(r => setTimeout(r, 1000));
+  // const prepareInterstitial = async (interstitialPlacementId: PlacementId) => {
+  //   for (let i = 0; i < 1; i++) {
+  //     await isInterstitialReady(interstitialPlacementId);
+  //     console.log("try count", i);
+  //     if (i > 20 || adsReadyFlag) {
+  //       break;
+  //     }
+  //     loadInterstitial(interstitialPlacementId);
+  //     await new Promise((r) => setTimeout(r, 500));
+  //   }
 
-    }
-  }
+  //   if (
+  //     Number(userState.userMemberExpired) <=
+  //       Number(userState.userCurrentTimestamp) ||
+  //     userState.userToken === ""
+  //   ) {
+  //     // not member, then render
+  //     isAdsShown = true;
+  //     renderInterstitial(currentRoute ?? "");
+  //   } else {
+  //     console.log("you are vip");
+  //     isAdsShown = true;
+  //     renderInterstitial(currentRoute ?? "");
+  //   }
+  // };
 
-  const renderInterstitial = (route: string) =>{ 
-    if (Number(userState.userMemberExpired) <= Number(userState.userCurrentTimestamp) || userState.userToken === ""){
-      // check is moved here to prevent user from seeing ads after subscribing to vip (same session)
-      // if not member, only run 
-      if (route === '首页'){
-        if (adsReadyFlag){
-          showInterstitial(Platform.OS === 'android' ? ANDROID_HOME_PAGE_POP_UP_ADS : IOS_HOME_PAGE_POP_UP_ADS)
-        }
-        else {
-          prepareInterstitial(Platform.OS === 'android' ? ANDROID_HOME_PAGE_POP_UP_ADS : IOS_HOME_PAGE_POP_UP_ADS)
-        } 
-      }
-  }
-  }
+  // const renderInterstitial = (route: string) => {
+  //   console.log("renderInterstitial");
+  //   if (route === "首页") {
+  //     if (adsReadyFlag) {
+  //       showInterstitial(
+  //         Platform.OS === "android"
+  //           ? ANDROID_HOME_PAGE_POP_UP_ADS
+  //           : IOS_HOME_PAGE_POP_UP_ADS
+  //       );
+  //     } else {
+  //       console.log("else");
+
+  //       // prepareInterstitial(
+  //       //   Platform.OS === "android"
+  //       //     ? ANDROID_HOME_PAGE_POP_UP_ADS
+  //       //     : IOS_HOME_PAGE_POP_UP_ADS
+  //       // );
+  //     }
+  //   }
+  // };
 
   useEffect(() => {
-      renderInterstitial(currentRoute ?? '');
-  }, [adsReadyFlag, currentRoute])
+    reTryLoad += 1;
+    loadInterstitial(
+      Platform.OS === "android"
+        ? ANDROID_HOME_PAGE_POP_UP_ADS
+        : IOS_HOME_PAGE_POP_UP_ADS
+    );
 
-  useEffect(() => {
-    if (currentRoute){
-      if (!visitCount[currentRoute]){
-        visitCount[currentRoute] = 0
-        setVisitCount({...visitCount})
-      }
-      visitCount[currentRoute] += 1; 
-      setVisitCount({...visitCount})
+    setTimeout(() => {
+      showInterstitial(
+        Platform.OS === "android"
+          ? ANDROID_HOME_PAGE_POP_UP_ADS
+          : IOS_HOME_PAGE_POP_UP_ADS
+      );
+    }, 400);
 
-      // // repeat this line for each page to customize their repeat logic... 
-      // if (currentRoute === '首页'){ 
-      //   if (visitCount[currentRoute] > 1){
-      //     setAdsReadyFlag(false)
-      //   }
-      // }
+    //   if (
+    //     Number(userState.userMemberExpired) <=
+    //       Number(userState.userCurrentTimestamp) ||
+    //     userState.userToken === ""
+    //   ) {
+    //     // not member, then render
+    //     renderInterstitial(currentRoute ?? "");
+    //   }
 
-    }
-  }, [currentRoute])
+    //}, [adsReadyFlag, currentRoute]);
+  }, []);
 
-  return; 
+  // useEffect(() => {
+  //   if (currentRoute){
+  //     if (!visitCount[currentRoute]){
+  //       visitCount[currentRoute] = 0
+  //       setVisitCount({...visitCount})
+  //     }
+  //     visitCount[currentRoute] += 1;
+  //     setVisitCount({...visitCount})
 
-}
+  //     // // repeat this line for each page to customize their repeat logic...
+  //     // if (currentRoute === '首页'){
+  //     //   if (visitCount[currentRoute] > 1){
+  //     //     setAdsReadyFlag(false)
+  //     //   }
+  //     // }
+
+  //   }
+  // }, [currentRoute])
+
+  return;
+};
 
 export default useInterstitialAds;
