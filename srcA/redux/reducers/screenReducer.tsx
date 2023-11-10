484df3d6b47631af1a356fb715aafdd5317d55c9
supermeng <supermeng@redfin.tech>
2023-11-10 15:09:39 +0800

    solve ads crash

--- srcA/redux/reducers/screenReducer.tsx
+++ srcA/redux/reducers/screenReducer.tsx
@@ -3,10 +3,10 @@ export interface screenActionType {
   payload: string;
 }
 
-import {screenModel} from '../../types/screenType';
+import { screenModel } from "../../types/screenType";
 
 const initialState: screenModel = {
-  screenAction: '',
+  screenAction: "",
   screenShow: false,
   loginShow: false,
   registerShow: false,
@@ -14,82 +14,93 @@ const initialState: screenModel = {
   resetInputForm: false,
   navigateToProfile: false,
   sportWatchTime: 0,
+  interstitialShow: false,
 };
 
 export function screenReducer(state = initialState, action: screenActionType) {
   switch (action.type) {
-    case 'remove_screen_action':
+    case "remove_screen_action":
       return {
         ...state,
         screenShow: false,
       };
-    case 'change_screen_action':
+    case "change_screen_action":
       return {
         ...state,
         screenShow: true,
         screenAction: action.payload,
       };
-    case 'show_login_action':
+    case "show_login_action":
       return {
         ...state,
         loginShow: true,
       };
-    case 'hide_login_action':
+    case "hide_login_action":
       return {
         ...state,
         loginShow: false,
       };
-    case 'show_register_action':
+    case "show_register_action":
       return {
         ...state,
         registerShow: true,
       };
-    case 'hide_register_action':
+    case "hide_register_action":
       return {
         ...state,
         registerShow: false,
       };
-    case 'hide_bottom_sheet_action':
+    case "hide_bottom_sheet_action":
       return {
         ...state,
         resetBottomSheet: true,
       };
-    case 'reset_bottom_sheet_action':
+    case "reset_bottom_sheet_action":
       return {
         ...state,
         resetBottomSheet: false,
       };
 
-    case 'initial_bottom_sheet_from':
+    case "initial_bottom_sheet_from":
       return {
         ...state,
         resetInputForm: false,
       };
-    case 'reset_bottom_sheet_form':
+    case "reset_bottom_sheet_form":
       return {
         ...state,
         resetInputForm: true,
       };
-    case 'navigate_to_profile_screen':
+    case "navigate_to_profile_screen":
       return {
         ...state,
         navigateToProfile: true,
       };
-    case 'reset_profile_screen':
+    case "reset_profile_screen":
       return {
         ...state,
         navigateToProfile: false,
       };
-    case 'increment_sport_watch_time': 
+    case "increment_sport_watch_time":
       return {
         ...state,
-        sportWatchTime: state.sportWatchTime + 1, 
-      }
-    case 'reset_sport_watch_time': 
-      return{
-        ...state, 
-        sportWatchTime: 0, 
-      }
+        sportWatchTime: state.sportWatchTime + 1,
+      };
+    case "reset_sport_watch_time":
+      return {
+        ...state,
+        sportWatchTime: 0,
+      };
+    case "interstitial_show":
+      return {
+        ...state,
+        interstitialShow: true,
+      };
+    case "interstitial_close":
+      return {
+        ...state,
+        interstitialShow: false,
+      };
     default:
       return state;
   }
