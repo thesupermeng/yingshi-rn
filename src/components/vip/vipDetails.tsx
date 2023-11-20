305e0f750569948663ebaa5294b68df9a2dfd241
supermeng <supermeng@redfin.tech>
2023-11-20 18:03:34 +0800

    【影视】【VIP明细】内容可居中，‘购买总额（USD)’可为一行

--- src/components/vip/vipDetails.tsx
+++ src/components/vip/vipDetails.tsx
@@ -9,16 +9,24 @@ import { useEffect, useState } from "react";
 import { VipPurchaseHistory } from "./vipPurchaseHistory";
 import { VipInviteHistory } from "./vipInviteHistory";
 
-export const VipDetails = ({ route, navigation }: RootStackScreenProps<"VIP明细">) => {
+export const VipDetails = ({
+  route,
+  navigation,
+}: RootStackScreenProps<"VIP明细">) => {
   const { textVariants, colors } = useTheme();
   const locations = [0, 1];
-  const navOptions = [{ "id": 0, "name": "邀请记录" }, { "id": 1, "name": "购买记录" }];
+  const navOptions = [
+    { id: 0, name: "邀请记录" },
+    { id: 1, name: "购买记录" },
+  ];
   const { userState } = route.params;
   const [accumulatedVipDays, setAccumulatedVipDays] = useState(0);
 
   useEffect(() => {
     let totalVipDays;
-    totalVipDays = userState.userAccumulateRewardDay + userState.userPaidVipList.total_purchased_days;
+    totalVipDays =
+      userState.userAccumulateRewardDay +
+      userState.userPaidVipList.total_purchased_days;
     setAccumulatedVipDays(totalVipDays);
   }, []);
 
@@ -26,44 +34,52 @@ export const VipDetails = ({ route, navigation }: RootStackScreenProps<"VIP明�
     <ScreenContainer>
       <TitleWithBackButtonHeader title="VIP明细" />
       <LinearGradient
-        colors={['#323638', '#1a1d20']} // An array of gradient colors
+        colors={["#323638", "#1a1d20"]} // An array of gradient colors
         locations={locations}
-        style={styles.card}>
+        style={styles.card}
+      >
         <View style={styles.featureItem}>
           <Text style={{ ...textVariants.bigHeader }}>
-            {accumulatedVipDays ? `${accumulatedVipDays}天` : '-'} 
+            {accumulatedVipDays ? `${accumulatedVipDays}天` : "-"}
           </Text>
-          <Text style={{ ...textVariants.small, color: colors.muted }}>累计VIP天数</Text>
+          <Text style={{ ...textVariants.small, color: colors.muted }}>
+            累计VIP天数
+          </Text>
         </View>
 
         <View style={styles.featureItem}>
           <Text style={{ ...textVariants.bigHeader }}>
-            {userState.userTotalInvite ? userState.userTotalInvite : '-'}
+            {userState.userTotalInvite ? userState.userTotalInvite : "-"}
           </Text>
-          <Text style={{ ...textVariants.small, color: colors.muted }}>已邀请人数</Text>
+          <Text style={{ ...textVariants.small, color: colors.muted }}>
+            已邀请人数
+          </Text>
         </View>
 
         <View style={styles.featureItem}>
           <Text style={{ ...textVariants.bigHeader }}>
-            {userState.userPaidVipList.total_purchased_amount ? userState.userPaidVipList.total_purchased_amount : '-'}
+            {userState.userPaidVipList.total_purchased_amount
+              ? userState.userPaidVipList.total_purchased_amount
+              : "-"}
           </Text>
-          <Text style={{ ...textVariants.small, color: colors.muted }}>购买总额 （USD）</Text>
+          <Text style={{ ...textVariants.small, color: colors.muted }}>
+            购买总额 （USD）
+          </Text>
         </View>
       </LinearGradient>
       <VipNav
-        tabList={
-          navOptions.map((e) => ({
-            id: e.id,
-            title: e.name,
-            name: e.name,
-          }))
-        }
+        tabList={navOptions.map((e) => ({
+          id: e.id,
+          title: e.name,
+          name: e.name,
+        }))}
         tabChildren={(tab) => (
           <>
-            {tab.name === '购买记录' ?
-              <VipPurchaseHistory userState={userState} /> :
+            {tab.name === "购买记录" ? (
+              <VipPurchaseHistory userState={userState} />
+            ) : (
               <VipInviteHistory userState={userState} />
-            }
+            )}
           </>
         )}
       />
@@ -73,18 +89,19 @@ export const VipDetails = ({ route, navigation }: RootStackScreenProps<"VIP明�
 
 const styles = StyleSheet.create({
   card: {
-    paddingHorizontal: 20,
+    paddingLeft: 20,
+    paddingRight: 0,
     paddingVertical: 15,
     marginHorizontal: 5,
     marginTop: 30,
     borderRadius: 15,
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
+    flexDirection: "row",
+    justifyContent: "space-evenly",
   },
 
   featureItem: {
     flex: 1,
-    alignItems: 'flex-start',
+    alignItems: "flex-start",
     gap: 5,
     marginBottom: 10,
     marginTop: 10,
