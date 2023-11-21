bd7b56646145a4287d3feee3949654b26f636645
kelvin <kelvin@redfin.tech>
2023-11-21 11:18:53 +0800

    done search analytics

--- src/components/vod/vodWithDescriptionList.tsx
+++ src/components/vod/vodWithDescriptionList.tsx
@@ -1,34 +1,38 @@
 import React, { useCallback, useEffect, useState, memo } from 'react';
-import {View, FlatList, Text, StyleSheet} from 'react-native';
-import {useNavigation, useTheme} from '@react-navigation/native';
+import { View, FlatList, Text, StyleSheet } from 'react-native';
+import { useNavigation, useTheme } from '@react-navigation/native';
 
-import {VodType} from '../../types/ajaxTypes';
-import {useAppDispatch} from '../../hooks/hooks';
+import { VodType } from '../../types/ajaxTypes';
+import { useAppDispatch } from '../../hooks/hooks';
 import FavoriteVodCard from './favoriteVodCard';
-import {playVod} from '../../redux/actions/vodActions';
+import { playVod } from '../../redux/actions/vodActions';
+import useAnalytics from '../../hooks/useAnalytics';
 
 interface Props {
   vodList: Array<VodType>;
+  onViewSearchResult?: () => void;
 }
 
 type FlatListType = {
   item: VodType;
-  index: number; 
+  index: number;
 };
 
-function VodWithDescriptionList ({vodList}: Props) {
+function VodWithDescriptionList({ vodList, onViewSearchResult }: Props) {
   const dispatch = useAppDispatch();
   const navigation = useNavigation();
-  const {spacing, textVariants, colors} = useTheme();
+  const { spacing, textVariants, colors } = useTheme();
 
   const renderItem = useCallback( // optimisation as per documentation
-    ({item, index}: FlatListType) => (
+    ({ item, index }: FlatListType) => (
       <FavoriteVodCard
         hideFavoriteButton={true}
         vod={item}
         onPress={() => {
           dispatch(playVod(item));
-          navigation.navigate('播放', {vod_id: item.vod_id});
+          navigation.navigate('播放', { vod_id: item.vod_id });
+
+          if (onViewSearchResult !== undefined) onViewSearchResult();
         }}
         index={index}
       />
@@ -38,27 +42,27 @@ function VodWithDescriptionList ({vodList}: Props) {
   if (vodList.length > 0) { // under normal condition, render the flatlist
     return (
       <View>
-          <FlatList
-            data={vodList}
-            contentContainerStyle={{paddingBottom: 200}}
-            renderItem={renderItem}
-            ListFooterComponent={
-              <View style={{...styles.loading, marginBottom: 100}}>
-                <Text
-                  style={{
-                    ...textVariants.subBody,
-                    color: colors.muted,
-                    paddingTop: 12,
-                    paddingBottom: 20,
-                  }}>
-                  没有更多了
-                </Text>
-              </View>
-            }
-            keyExtractor={(item, index)=>index.toString()} // optimisation as per documentation
-            initialNumToRender={5} // optimisation as per documentation
-            removeClippedSubviews={true} // optimisation as per documentation
-          />
+        <FlatList
+          data={vodList}
+          contentContainerStyle={{ paddingBottom: 200 }}
+          renderItem={renderItem}
+          ListFooterComponent={
+            <View style={{ ...styles.loading, marginBottom: 100 }}>
+              <Text
+                style={{
+                  ...textVariants.subBody,
+                  color: colors.muted,
+                  paddingTop: 12,
+                  paddingBottom: 20,
+                }}>
+                没有更多了
+              </Text>
+            </View>
+          }
+          keyExtractor={(item, index) => index.toString()} // optimisation as per documentation
+          initialNumToRender={5} // optimisation as per documentation
+          removeClippedSubviews={true} // optimisation as per documentation
+        />
       </View>
     );
   } else { // else dont render anything at all
--- srcA/components/vod/vodWithDescriptionList.tsx
+++ srcA/components/vod/vodWithDescriptionList.tsx
@@ -1,34 +1,37 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import {View, FlatList, Text, StyleSheet, Linking} from 'react-native';
-import {useNavigation, useTheme} from '@react-navigation/native';
+import { View, FlatList, Text, StyleSheet, Linking } from 'react-native';
+import { useNavigation, useTheme } from '@react-navigation/native';
 
-import {VodType} from '../../types/ajaxTypes';
-import {useAppDispatch} from '../../hooks/hooks';
+import { VodType } from '../../types/ajaxTypes';
+import { useAppDispatch } from '../../hooks/hooks';
 import FavoriteVodCard from './favoriteVodCard';
-import {playVod} from '../../redux/actions/vodActions';
+import { playVod } from '../../redux/actions/vodActions';
 
 interface Props {
   vodList: Array<VodType>;
+  onViewSearchResult?: () => void;
 }
 
 type FlatListType = {
   item: VodType;
-  index: number; 
+  index: number;
 };
 
-export default ({vodList}: Props) => {
+export default ({ vodList, onViewSearchResult }: Props) => {
   const dispatch = useAppDispatch();
   const navigation = useNavigation();
-  const {spacing, textVariants, colors} = useTheme();
+  const { spacing, textVariants, colors } = useTheme();
 
   const renderItem = useCallback( // optimisation as per documentation
-    ({item, index}: FlatListType) => (
+    ({ item, index }: FlatListType) => (
       <FavoriteVodCard
         hideFavoriteButton={true}
         vod={item}
         onPress={() => {
           dispatch(playVod(item));
-          navigation.navigate('播放', {vod_id: item.vod_id});
+          navigation.navigate('播放', { vod_id: item.vod_id });
+
+          if (onViewSearchResult !== undefined) onViewSearchResult();
         }}
         index={index}
       />
@@ -38,27 +41,27 @@ export default ({vodList}: Props) => {
   if (vodList.length > 0) { // under normal condition, render the flatlist
     return (
       <View>
-          <FlatList
-            data={vodList}
-            contentContainerStyle={{paddingBottom: 200}}
-            renderItem={renderItem}
-            ListFooterComponent={
-              <View style={{...styles.loading, marginBottom: 100}}>
-                <Text
-                  style={{
-                    ...textVariants.subBody,
-                    color: colors.muted,
-                    paddingTop: 12,
-                    paddingBottom: 20,
-                  }}>
-                  没有更多了
-                </Text>
-              </View>
-            }
-            keyExtractor={(item, index)=>index.toString()} // optimisation as per documentation
-            initialNumToRender={5} // optimisation as per documentation
-            removeClippedSubviews={true} // optimisation as per documentation
-          />
+        <FlatList
+          data={vodList}
+          contentContainerStyle={{ paddingBottom: 200 }}
+          renderItem={renderItem}
+          ListFooterComponent={
+            <View style={{ ...styles.loading, marginBottom: 100 }}>
+              <Text
+                style={{
+                  ...textVariants.subBody,
+                  color: colors.muted,
+                  paddingTop: 12,
+                  paddingBottom: 20,
+                }}>
+                没有更多了
+              </Text>
+            </View>
+          }
+          keyExtractor={(item, index) => index.toString()} // optimisation as per documentation
+          initialNumToRender={5} // optimisation as per documentation
+          removeClippedSubviews={true} // optimisation as per documentation
+        />
       </View>
     );
   } else { // else dont render anything at all
