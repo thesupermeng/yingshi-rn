7f614ab2ba22f48045679e30f6620ea08805a23b
carl <carl@redfin.tech>
2023-11-21 17:05:44 +0800

    refactor

--- src/screens/Playlist/AdultVideoList.tsx
+++ src/screens/Playlist/AdultVideoList.tsx
@@ -18,93 +18,92 @@ import ConfirmationModal from '../../components/modal/confirmationModal';
 import VodLiveStationListVertical from '../../components/vod/vodLiveStationListVertical';
 import EmptyList from '../../components/common/emptyList';
 import {ScrollView} from 'react-native-gesture-handler';
-import FastImage from '../../components/common/customFastImage'
+import FastImage from '../../components/common/customFastImage';
+import {API_DOMAIN_TEST} from '../../utility/constants';
 
 type AdultVodReturnType = {
   data: {
     List: Array<AdultVodType>;
     TotalPageCount: number;
-  }, 
-}
+  };
+};
 
 const fetchVod = async (page: number) => {
-  const data: AdultVodReturnType = await (await fetch('https://testapi.yingshi.tv/svod/v1/vod?' + new URLSearchParams({
-    page: page.toString(), 
-    limit: '10'
-  }))).json()
-  return data.data
-}
+  const data: AdultVodReturnType = await (
+    await fetch(
+      `${API_DOMAIN_TEST}svod/v1/vod?` +
+        new URLSearchParams({
+          page: page.toString(),
+          limit: '10',
+        }),
+    )
+  ).json();
+  return data.data;
+};
 
 export default ({navigation, route}: RootStackScreenProps<'午夜场剧情'>) => {
-  const [adultVodData, setAdultVodData] = useState<AdultVodType[]>([])
+  const [adultVodData, setAdultVodData] = useState<AdultVodType[]>([]);
   const {colors, textVariants, spacing, icons} = useTheme();
-  const [page, setPage] = useState(1)
-  const totalPageCount = useRef<number>(0)
-  const [hasNextPage, setHasNextPage] = useState(true)
-  const [isFetching, setIsFetching] = useState(false)
+  const [page, setPage] = useState(1);
+  const totalPageCount = useRef<number>(0);
+  const [hasNextPage, setHasNextPage] = useState(true);
+  const [isFetching, setIsFetching] = useState(false);
 
   const fetchNextPage = async () => {
-    if (page > totalPageCount.current) {
-      setHasNextPage(false)
-      return
+    if (page >= totalPageCount.current) {
+      setHasNextPage(false);
+      return;
     } else {
-      setHasNextPage(true)
+      setHasNextPage(true);
     }
-    setIsFetching(true)
-    const vodData = (await fetchVod(page))
-    const data = vodData.List
-    setIsFetching(false)
+    setIsFetching(true);
+    const vodData = await fetchVod(page);
+    const data = vodData.List;
+    setIsFetching(false);
 
-    setAdultVodData(curr => [...curr, ...data])
-    setPage(page => page + 1)
-  }
+    setAdultVodData(curr => [...curr, ...data]);
+    setPage(page => page + 1);
+  };
 
-  const renderItem = ({item, index}: {item:AdultVodType, index: number}) => (
+  const renderItem = ({item, index}: {item: AdultVodType; index: number}) => (
     <View
       style={{
-        width:"48%",
-        padding: 5, 
-        flex: 1
-      }}
-    >
-
-    <TouchableOpacity
-      key={item.vod_id}
-      style={styles.cardItem}
-      onPress={() => {
-        console.debug('vod pressed', item.vod_name)
-      }}
-    >
-      <FastImage
+        width: '48%',
+        padding: 5,
+        flex: 1,
+      }}>
+      <TouchableOpacity
+        key={item.vod_id}
+        style={styles.cardItem}
+        onPress={() => {
+          console.debug('vod pressed', item.vod_name);
+        }}>
+        <FastImage
           style={{flex: 1, borderRadius: 10}}
           source={{
-            uri: item.vod_pic_thumb
+            uri: item.vod_pic_thumb,
           }}
         />
-    </TouchableOpacity>
-    <View
-      style={{
-        width: '100%'
-      }}
-    >
-      <Text
-        style={styles.cardTitle}
-        numberOfLines={2}
-      >{item.vod_name}</Text>
-
+      </TouchableOpacity>
+      <View
+        style={{
+          width: '100%',
+        }}>
+        <Text style={styles.cardTitle} numberOfLines={2}>
+          {item.vod_name}
+        </Text>
+      </View>
     </View>
-    </View>
-
-  )
+  );
 
   useEffect(() => {
-    fetchVod(page).then((data) => {
-      totalPageCount.current = data.TotalPageCount
-      setAdultVodData(curr => [...curr, ...data.List])
-      setPage(page => page + 1)
-    })
-  }, [])
-  
+    fetchVod(page).then(data => {
+      totalPageCount.current = data.TotalPageCount;
+      setAdultVodData(curr => [...curr, ...data.List]);
+      setPage(page => page + 1);
+    });
+  }, []);
+
   return (
     <ScreenContainer>
       <TitleWithBackButtonHeader
@@ -116,10 +115,11 @@ export default ({navigation, route}: RootStackScreenProps<'午夜场剧情'>) =>
           data={adultVodData}
           renderItem={renderItem}
           numColumns={2}
-          contentContainerStyle={{justifyContent:'space-evenly'}}
+          contentContainerStyle={{justifyContent: 'space-evenly'}}
           onEndReached={fetchNextPage}
+          showsVerticalScrollIndicator={false}
           ListFooterComponent={
-            <View style={{ ...styles.loading, marginBottom: 60 }}>
+            <View style={{...styles.loading, marginBottom: 60}}>
               {hasNextPage && (
                 <FastImage
                   style={{
@@ -127,33 +127,30 @@ export default ({navigation, route}: RootStackScreenProps<'午夜场剧情'>) =>
                     width: 80,
 
                     flex: 1,
-                    justifyContent: "center",
-                    alignItems: "center",
+                    justifyContent: 'center',
+                    alignItems: 'center',
                   }}
-                  source={require("../../../static/images/loading-spinner.gif")}
-                  resizeMode={"contain"}
+                  source={require('../../../static/images/loading-spinner.gif')}
+                  resizeMode={'contain'}
                 />
               )}
-              {!(isFetching) && !hasNextPage && (
+              {!isFetching && page !== 1 && !hasNextPage && (
                 <Text
                   style={{
                     ...textVariants.subBody,
                     color: colors.muted,
                     paddingTop: 12,
-                  }}
-                >
+                  }}>
                   已经到底了
                 </Text>
               )}
             </View>
           }
         />
-
       </View>
     </ScreenContainer>
   );
 };
-
 
 const styles = StyleSheet.create({
   container: {
@@ -166,14 +163,14 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between', // Distribute items evenly in the row
     marginBottom: 10, // Add spacing between rows
-    backgroundColor: 'red'
+    backgroundColor: 'red',
   },
   cardItem: {
     // width: '100%', // Take up 48% of the available width (2 items per row with spacing)
     aspectRatio: 130 / 80,
     borderRadius: 10,
     // overflow: 'hidden', // Ensure content stays within borders
-    backgroundColor: 'green', 
+    backgroundColor: 'green',
   },
   loading: {
     flexDirection: 'row',
@@ -181,8 +178,7 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   cardTitle: {
-    color: '#FFF', 
-    textAlign: 'center', 
-
+    color: '#FFF',
+    textAlign: 'center',
   },
 });
