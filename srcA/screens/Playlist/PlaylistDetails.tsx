039164d4a2ea9171aec645a9fea3551fc6b67f86
supermeng <supermeng@redfin.tech>
2023-11-22 16:06:40 +0800

    add back fav playlist

--- srcA/screens/Playlist/PlaylistDetails.tsx
+++ srcA/screens/Playlist/PlaylistDetails.tsx
@@ -1,24 +1,27 @@
-import React, { useCallback, useEffect } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import ScreenContainer from '../../components/container/screenContainer';
-import { useTheme } from '@react-navigation/native';
-import { useAppSelector } from '../../hooks/hooks';
-import { RootState } from '../../redux/store';
-import TitleWithBackButtonHeader from '../../components/header/titleWithBackButtonHeader';
-import FavoritePlaylistButton from '../../components/button/favoritePlaylistButton';
-import { RootStackScreenProps } from '../../types/navigationTypes';
-import VodWithDescriptionList from '../../components/vod/vodWithDescriptionList';
-import useAnalytics from '../../hooks/useAnalytics';
+import React, { useCallback, useEffect } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import ScreenContainer from "../../components/container/screenContainer";
+import { useTheme } from "@react-navigation/native";
+import { useAppSelector } from "../../hooks/hooks";
+import { RootState } from "../../redux/store";
+import TitleWithBackButtonHeader from "../../components/header/titleWithBackButtonHeader";
+import FavoritePlaylistButton from "../../components/button/favoritePlaylistButton";
+import { RootStackScreenProps } from "../../types/navigationTypes";
+import VodWithDescriptionList from "../../components/vod/vodWithDescriptionList";
+import useAnalytics from "../../hooks/useAnalytics";
 
-export default ({ navigation }: RootStackScreenProps<'PlaylistDetail'>) => {
+export default ({ navigation }: RootStackScreenProps<"PlaylistDetail">) => {
   const { textVariants, colors, spacing } = useTheme();
   const playlistReducer = useAppSelector(
-    ({ vodPlaylistReducer }: RootState) => vodPlaylistReducer,
+    ({ vodPlaylistReducer }: RootState) => vodPlaylistReducer
   );
   const playlist = playlistReducer?.playlistDetails?.playlist;
 
   // ========== for analytics - start ==========
-  const { playlistTopicsViewsAnalytics, playlistTopicsClickAnalytics } = useAnalytics();
+  const {
+    playlistTopicsViewsAnalytics,
+    playlistTopicsClickAnalytics,
+  } = useAnalytics();
 
   useEffect(() => {
     if (playlist !== null) {
@@ -56,16 +59,21 @@ export default ({ navigation }: RootStackScreenProps<'PlaylistDetail'>) => {
                   color: colors.text,
                   flex: 1,
                   marginRight: spacing.l,
-                }}>
+                }}
+              >
                 {playlist?.topic_blurb}
               </Text>
-              {/* <FavoritePlaylistButton playlist={playlist} /> */}
+              <FavoritePlaylistButton playlist={playlist} />
             </View>
             <Text
               style={{
                 ...textVariants.small,
-              }}>{`(共${playlist.vod_list.length}部)`}</Text>
-            <VodWithDescriptionList vodList={playlist.vod_list} onClickCatalogVideo={onClickCatalogVideo} />
+              }}
+            >{`(共${playlist.vod_list.length}部)`}</Text>
+            <VodWithDescriptionList
+              vodList={playlist.vod_list}
+              onClickCatalogVideo={onClickCatalogVideo}
+            />
           </View>
         )}
       </ScreenContainer>
@@ -75,9 +83,9 @@ export default ({ navigation }: RootStackScreenProps<'PlaylistDetail'>) => {
 
 const styles = StyleSheet.create({
   header: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
   },
 });
