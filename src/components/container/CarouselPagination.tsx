8863ab5f16334280d43612f9483ccfa992967ed0
adrian <adrian@redfin.tech>
2023-11-23 18:02:39 +0800

    Bugfix Android no.43 improve carousel dot tracking

--- src/components/container/CarouselPagination.tsx
+++ src/components/container/CarouselPagination.tsx
@@ -1,12 +1,44 @@
-import React, { memo } from 'react';
-import {View, StyleSheet} from 'react-native';
+import React, { memo, useEffect, useRef, useState } from 'react';
+import {View, StyleSheet, TouchableOpacity} from 'react-native';
 
 interface Props {
   data?: any;
   activeIndex?: any;
+  onDotPress?: (index: number) => void;
+  onScrollIndexChanged?: (index: number) => void; // Callback to track scroll changes
 }
 
-const CarouselPagination = ({data, activeIndex}: Props) => {
+const CarouselPagination = ({data, activeIndex, onDotPress, onScrollIndexChanged,}: Props) => {
+
+  const scrollOffsetRef = useRef<number>(0);
+  const [debouncedActiveIndex, setDebouncedActiveIndex] = useState<number | null>(null);
+  const carouselActiveIndex = useRef<number | null>(null);
+
+   // Debounce the active index update to reduce rapid updates
+   useEffect(() => {
+    const debounceTimeout = setTimeout(() => {
+      setDebouncedActiveIndex(activeIndex);
+    }, 10); // Adjust the debounce delay as needed
+
+    return () => clearTimeout(debounceTimeout);
+  }, [activeIndex]);
+
+  // Effect to handle the updated active index
+  useEffect(() => {
+    if (debouncedActiveIndex !== null) {
+      carouselActiveIndex.current = debouncedActiveIndex; // Update ref with debounced active index
+      if (onScrollIndexChanged) {
+        onScrollIndexChanged(debouncedActiveIndex); // Trigger function for updated index
+      }
+    }
+  }, [debouncedActiveIndex, onScrollIndexChanged]);
+
+  // useEffect(() => {
+  //   // Handle the carousel scroll change event
+  //   if (onScrollIndexChanged) {
+  //     onScrollIndexChanged(scrollOffsetRef.current);
+  //   }
+  // }, [onScrollIndexChanged]);
 
   const currentItem = (item: any, index: number) => (
     <View
@@ -15,7 +47,17 @@ const CarouselPagination = ({data, activeIndex}: Props) => {
         styles.paginationDot,
         index === activeIndex && styles.paginationDotActive,
       ]}
+      onTouchStart={() => onDotPress && onDotPress(index)} // Trigger function on dot press
     />
+
+    // <TouchableOpacity
+    //   key={index}
+    //   style={[
+    //     styles.paginationDot,
+    //     index === activeIndex && styles.paginationDotActive,
+    //   ]}
+    //   onPress={() => onDotPress && onDotPress(index)} // Trigger function on dot press
+    // />
   )
 
   return (
