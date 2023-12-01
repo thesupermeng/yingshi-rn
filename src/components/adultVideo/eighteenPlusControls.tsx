0446ff6905819a2b984491301185924977e31c53
carl <carl@redfin.tech>
2023-12-01 09:17:42 +0800

    fix indicator show when not 18+ mode watch anytime

--- src/components/adultVideo/eighteenPlusControls.tsx
+++ src/components/adultVideo/eighteenPlusControls.tsx
@@ -26,7 +26,7 @@ const eighteenPlusControls = ({}: Props) => {
   const userState: userModel = useAppSelector(
     ({userReducer}) => userReducer
   )
-  const {adultModeVipShow, adultModeDisclaimerShow, adultMode} = screenState
+  const {adultModeVipShow, adultModeDisclaimerShow, adultMode, watchAnytimeAdultMode} = screenState
   const dispatch = useAppDispatch()
   // const isVip = !(Number(userState.userMemberExpired) <=
   //                 Number(userState.userCurrentTimestamp) ||
@@ -49,14 +49,15 @@ const eighteenPlusControls = ({}: Props) => {
       <AdultModeSwitch 
       switchStyle={styles.switch}        
       />
-      <AdultModeCountdownIndicator
+      {watchAnytimeAdultMode && 
+        <AdultModeCountdownIndicator
         containerStyle={{
           position: 'absolute', 
           top: 70, 
           right: 20, 
           flex: 1
         }}
-      />
+      />}
       <EighteenPlusOverlay
         handleAccept={() => {
           console.debug('accepted 18+')
