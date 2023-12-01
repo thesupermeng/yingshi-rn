8b3f728ecb3445d7e024b78dff21b129b9f6f6ab
juicy <juicy@redfin.tech>
2023-12-01 17:53:27 +0800

    Make x vod image longer

--- src/components/container/CatagoryHome.tsx
+++ src/components/container/CatagoryHome.tsx
@@ -134,7 +134,7 @@ const CatagoryHome = ({
         /> 
         { navId == 99 
           ? // is 午夜场剧情
-          item?.vod_list && <VodListVertical numOfRows={2} vods={item?.vod_list} minNumPerRow={2} heightToWidthRatio={1/1.414} playerMode='adult' />
+          item?.vod_list && <VodListVertical numOfRows={2} vods={item?.vod_list} minNumPerRow={2} heightToWidthRatio={1/1.814} playerMode='adult' />
           : 
           item?.vod_list && <VodListVertical vods={item?.vod_list}/>
         }
