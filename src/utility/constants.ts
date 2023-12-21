8fe7143266b56dd2f942d5155058f8fae7b99757
supermeng <supermeng@redfin.tech>
2023-12-22 00:47:28 +0800

    hide payment

--- src/utility/constants.ts
+++ src/utility/constants.ts
@@ -114,7 +114,7 @@ export const NON_VIP_STREAM_TIME_SECONDS = 300;
 
 export const PLAY_HTTP_SERVER_PORT = 8085;
 
-export const SHOW_ZF_CONST = true;
+export const SHOW_ZF_CONST = false;
 
 export const ADULT_MODE_PREVIEW_DURATION = 300; // number of seconds 
 
