f61e4c6b19a83eeb7a8ec466e3195099c5ad7edc
supermeng <supermeng@redfin.tech>
2023-12-28 19:26:18 +0800

    vod v3

--- src/constants/api.tsx
+++ src/constants/api.tsx
@@ -18,7 +18,7 @@ export class CEndpoint {
     static vodGetTopicType = 'type/v1/type';
     static vodGetDetail = 'vod/v3/vod/detail';
     static vodGetXDetail = 'svod/v1/vod/detail';
-    static vodGetList = 'vod/v2/vod';
+    static vodGetList = 'vod/v3/vod';
     static vodGetXList = 'svod/v1/vod';
     static vodGetReviewDetail = 'vod/v1/vod/reviewdetail';
     static vodGetAdsSlot = 'ads/v1/slot';
