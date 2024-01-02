7e44dee20643334cd5172de5d86d28a27047d398
kelvin <kelvin@redfin.tech>
2024-01-02 13:11:45 +0800

    remove a screen ios outside the src

--- aScreen.ios/Sports/utility/hash_util.js
+++
@@ -1,6 +0,0 @@
-import md5 from 'crypto-js/md5';
-export const addHeaderSignature = (params, timestamp, salt = 'salt') => {
-  const keys = Object.keys(params || {}).sort();
-  const end = `${keys?.map(k => params[k]).join('')}${timestamp}${salt}`;
-  return md5(end).toString();
-};
