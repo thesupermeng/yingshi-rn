1fc3ac60b8541d9d6689a128364e82b567871938
carl <carl@redfin.tech>
2024-01-08 15:52:09 +0800

    add alternative for image loading

--- src/components/common/customFastImage.tsx
+++ src/components/common/customFastImage.tsx
@@ -1,3 +1,4 @@
+import { useRef, useState } from "react";
 import { Image, ImageURISource, LogBox } from "react-native";
 import { Platform } from "react-native";
 import { ImageProps, ViewProps } from "react-native"
@@ -5,18 +6,36 @@ import FastImage, { FastImageProps } from "react-native-fast-image";
 
 type MyImageProp = ImageProps & {source: ImageURISource}
 
-type Prop = FastImageProps & {useFastImage?: boolean}
+type Prop = FastImageProps & {useFastImage?: boolean, alternativeImg?: string[]}
 
 LogBox.ignoreLogs([`ReactImageView: Image source "null" doesn't exist`])
 
-const customFastImage = ({useFastImage = false, ...imageProp}: Prop) => {
+const customFastImage = ({useFastImage = false, alternativeImg, ...imageProp}: Prop) => {
+  const currentImg = useRef(0); 
+  const [imgUrl, setImgUrl] = useState(typeof imageProp.source == 'number' ? undefined :  imageProp.source?.uri)
+
+  const useNextImage = () => {
+    if (!alternativeImg || alternativeImg.length <= 0) return undefined
+    currentImg.current += 1; 
+    if (currentImg.current >= alternativeImg.length) return undefined
+    return alternativeImg[currentImg.current]
+  }
+
+  const handleOnError = () => {
+    setImgUrl(useNextImage())
+  }
+
   if (useFastImage === true || Platform.OS === "android"){
+    if (typeof imageProp.source == 'number'){ // if source={require(...)}
     return <FastImage {...imageProp as FastImageProps}/>
+    } else {
+      return <FastImage {...imageProp as FastImageProps} source={{uri: imgUrl}} onError={handleOnError}/>
+    }
   } else {
     if (typeof imageProp.source == 'number'){ // if source={require(...)}
       return <Image {...imageProp as MyImageProp}/>
     } else { // if source={{uri:...}}
-      return <Image {...imageProp as MyImageProp} source={{...(imageProp.source as ImageURISource), uri: !!(imageProp as MyImageProp).source.uri ? (imageProp as MyImageProp).source.uri : undefined}}/>
+      return <Image {...imageProp as MyImageProp} source={{...(imageProp.source as ImageURISource), uri: imgUrl}} onError={handleOnError}/>
     }
   }
 }
