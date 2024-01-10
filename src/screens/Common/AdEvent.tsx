4c1641999af4583dc20f13a8954bc2ff91e8373c
juicy <juicy@redfin.tech>
2024-01-10 16:14:15 +0800

    Fixed AdEvent back

--- src/screens/Common/AdEvent.tsx
+++ src/screens/Common/AdEvent.tsx
@@ -7,9 +7,11 @@ import {
   View,
   Text,
 } from "react-native";
+import React, { useEffect, useRef, useState } from "react";
 
 const AdEvent = ({ navigation, route }: RootStackScreenProps<'活动页'>) => {
   const {bannerAd} = route.params
+  const [isNavigated, setIsNavigated] = useState(false);
   
   const handleOnMessage = (message: string) => {
     console.debug(message)
@@ -17,20 +19,29 @@ const AdEvent = ({ navigation, route }: RootStackScreenProps<'活动页'>) => {
       navigation.navigate('付费VIP')
     }
   }
+  const webViewref = useRef<any>();
 
   return (
     <ScreenContainer>
       <TitleWithBackButtonHeader
         title={bannerAd.ads_name}
+        onBack={() => isNavigated ? webViewref.current.goBack() : navigation.goBack()}
       />
       <WebView
           source={{
             uri: bannerAd.ads_url
           }}
+          ref={webViewref}
           style={{ backgroundColor: 'transparent'}}
           onShouldStartLoadWithRequest={() => true}
           onMessage={m => handleOnMessage(m.nativeEvent.data)}
-
+          onNavigationStateChange={(event) => {
+            if (event.url === bannerAd.ads_url) {
+              setIsNavigated(false);
+            } else {
+              setIsNavigated(true);
+            }
+          }}
         />
 
     </ScreenContainer>
