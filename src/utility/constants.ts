0a71d588ba39b4875e98b553d757f4f7a08e1b3e
carl <carl@redfin.tech>
2024-01-15 17:04:13 +0800

    change aimeiju to yinghsi

--- src/utility/constants.ts
+++ src/utility/constants.ts
@@ -107,9 +107,9 @@ export const AMJ_PRODUCT_IOS = 6;
 export const IS_IOS = Platform.OS === 'ios';
 export const IS_ANDROID = Platform.OS === 'android';
 
-export const APP_NAME_CONST = '爱美剧';
+export const APP_NAME_CONST = '影视TV';
 export const APP_NAME_CONST2 = '该APP';
-export const APP_EMAIL_CONST = 'contact.movie9@gmail.com';
+export const APP_EMAIL_CONST = 'contactus@yingshi.tv';
 
 export const NON_VIP_STREAM_TIME_SECONDS = 300;
 
