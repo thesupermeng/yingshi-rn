f728f4cfc8e7db9a3cfa79749bba9390a7abd7c2
carl <carl@redfin.tech>
2024-01-17 16:22:20 +0800

    done download using redux (basic)

--- src/redux/actions/videoDownloadAction.ts
+++ src/redux/actions/videoDownloadAction.ts
@@ -1,14 +1,11 @@
 import { DownloadVideoActionType } from "@type/actionTypes";
 import { VodType } from "@type/ajaxTypes";
-import { downloadVodImage } from "../../utils/vodDownloader";
+import { downloadVod, downloadVodImage } from "../../utils/vodDownloader";
 import { ThunkAction } from "redux-thunk";
 import { DownloadVideoReducerState } from "@type/vodDownloadTypes";
 import { RootState } from "@redux/store";
 
 function addVideoToDownload(vod: VodType, vodSourceId: number, vodUrlNid: number): DownloadVideoActionType {
-  // TODO : add download for vod image 
-  downloadVodImage(vod)
-
   return {
     type: 'ADD_VIDEO_TO_DOWNLOAD', 
     payload: {
@@ -19,14 +16,45 @@ function addVideoToDownload(vod: VodType, vodSourceId: number, vodUrlNid: number
   }
 }
 
+// function updateImagePath(vod: VodType, filepath: string): DownloadVideoActionType{
+
+//   return {
+//     type: 'UPDATE_VOD_IMAGE', 
+//     payload: {
+//       vod: vod, 
+//       vodSourceId: NaN, // doesnt matter
+//       vodUrlNid: NaN, // doesnt matter 
+//       imagePath: filepath
+//     }
+//   }
+// }
+
+function getUrlOfVod(vod: VodType, vodSourceId: number, vodUrlNid: number) {
+  console.log(vod)
+  return vod.vod_sources
+    .find(source => source.source_id === vodSourceId)?.vod_play_list.urls
+    .find(url => url.nid === vodUrlNid)
+    ?.url
+
+}
+
 export function addVideoToDownloadThunk(
   vod: VodType,
   vodSourceId: number,
   vodUrlNid: number,
 ): ThunkAction<void, RootState, any, DownloadVideoActionType> {
   return async function (dispatch, getState) {
-    await downloadVodImage(vod);
     dispatch(addVideoToDownload(vod, vodSourceId, vodUrlNid));
+    await downloadVodImage(vod);
+    // here can dispatch updateImagePath () //
+    
+    downloadVod(
+      `${vod.vod_id}-${vodSourceId}-${vodUrlNid}`, 
+      getUrlOfVod(vod, vodSourceId, vodUrlNid), 
+      ({percentage}) => {console.debug('downloaded', percentage)}, 
+      () => console.debug('download complete for ', vod.vod_name), 
+      () => console.debug('error downloading ', vod.vod_name)
+    )
 
   };
 }
