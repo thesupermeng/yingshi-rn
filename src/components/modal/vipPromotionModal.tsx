94e2b6189cf529eac8864ff0999ed2d26096509e
elena <elena@redfin.tech>
2024-01-29 09:53:37 +0800

    vip promotion overlay

---
+++ src/components/modal/vipPromotionModal.tsx
@@ -0,0 +1,286 @@
+import React, { useEffect, ReactNode, useState } from "react";
+import { View, Text, TouchableOpacity, StyleSheet, Dimensions } from "react-native";
+import FastImage from "../common/customFastImage";
+import LinearGradient from "react-native-linear-gradient";
+
+interface Props {
+  coverImage: any;
+  coverBackground: any;
+  onClose: any;
+  onPurchase: any;
+  showCondition: boolean;
+}
+
+export default function VipPromotionModal({
+  coverImage,
+  coverBackground,
+  onClose,
+  onPurchase,
+  showCondition,
+}: Props) {
+  const isFullscreen = Dimensions.get('window').height < Dimensions.get('window').width;
+  const countdownVal = [0, 0, 5, 9, 5, 9];
+  const targetDate = '2024-02-01T00:56:59Z';
+  const [remainingTime, setRemainingTime] = useState<string[]>(['0', '0', '0', '0', '0', '0']);
+
+  useEffect(() => {
+    const calCountdownDays = () => {
+      const now = new Date();
+      const target = new Date(targetDate);
+  
+      let diff = target.getTime() - now.getTime();
+  
+      if (diff < 0) {
+        setRemainingTime(['0', '0', '0', '0', '0', '0']) ;
+      }
+  
+      const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+      diff -= days * (1000 * 60 * 60 * 24);
+  
+      const hours = Math.floor(diff / (1000 * 60 * 60));
+      diff -= hours * (1000 * 60 * 60);
+  
+      const minutes = Math.floor(diff / (1000 * 60));
+      diff -= minutes * (1000 * 60);
+  
+      const seconds = Math.floor(diff / 1000);
+  
+      const formattedDays = String(days).padStart(2, '0');
+      const formattedHours = String(hours).padStart(2, '0');
+      const formattedMinutes = String(minutes).padStart(2, '0');
+      const formattedSeconds = String(seconds).padStart(2, '0');
+      console.log(formattedDays)
+      console.log(formattedHours)
+      console.log(formattedMinutes)
+  
+      setRemainingTime([...formattedDays.split(''), ...formattedHours.split(''), ...formattedMinutes.split('')]);
+    }
+
+    const intervalId = setInterval(() => {
+      calCountdownDays()
+    }, 60000);
+
+    return () => clearInterval(intervalId);
+  }, [targetDate]);
+
+  if (showCondition)
+    return (
+      <View
+        style={{
+          position: "absolute",
+          width: "100%",
+          height: "100%",
+          transform: isFullscreen ? [{ scale: 0.75 }] : []
+        }}
+      >
+        <View
+          style={{
+            flex: 1,
+            position: "relative",
+            alignItems: "center",
+            justifyContent: "center",
+            alignSelf: "center",
+            alignContent: "center",
+          }}
+        >
+          <LinearGradient
+            colors={['#4A3E2A', '#231D14', '#1A1712', '#191612']}
+            locations={[0, 0.29, 0.63, 1]}
+            style={{
+              width: 322,
+              height: 377,
+              borderRadius: 14,
+              paddingHorizontal: 16,
+              paddingBottom: 10,
+            }}
+            start={{ x: 0, y: 0 }}
+            end={{ x: 0, y: 1 }}
+          >
+            <View style={{
+              position: "relative",
+              alignItems: "center",
+              paddingVertical: 15,
+            }}>
+              <FastImage
+                source={coverBackground}
+                style={styles.image2}
+                resizeMode="center"
+              />
+              <FastImage
+                source={coverImage}
+                style={styles.image1}
+                resizeMode="contain"
+              />
+            </View>
+
+            <View
+              style={{
+                flex: 1,
+                gap: 16,
+                marginTop: 25,
+                flexDirection: 'column',
+              }}>
+              <View
+                style={{
+                  gap: 10,
+                  flexDirection: 'column',
+                }}>
+                <Text
+                  style={{
+                    ...styles.titleText
+                  }}>
+                  限时优惠
+                </Text>
+                <View style={styles.countdownContainer}>
+                  {remainingTime.map((val, i) => {
+                    return (
+                      <>
+                        <View
+                          key={i}
+                          style={{
+                            paddingVertical: 2,
+                            paddingHorizontal: 10,
+                            backgroundColor: 'rgba(255, 255, 255, 0.10)',
+                            borderRadius: 6,
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                          }}
+                        >
+                          <Text style={styles.countdownText}>
+                            {val}
+                          </Text>
+
+                        </View>
+                        {i % 2 === 1 && i < countdownVal.length - 1 && (
+                          <Text style={{ ...styles.countdownText, padding: 1, }}>
+                            :
+                          </Text>
+                        )}
+                      </>
+                    );
+                  })}
+                </View>
+              </View>
+              <Text style={styles.contentText1}>
+                限时优惠，立即升级会员可享受最低4折优惠，先到先得！已有99.5%用户抢先购买，解锁了更多影视权益。您确定要错过这个升级体验的最好机会吗？
+              </Text>
+              <Text style={styles.contentText2}>
+                限时优惠
+                <Text style={{ ...styles.contentText2, color: '#FAC33D' }}>5万</Text>名额，已有
+                <Text style={{ ...styles.contentText2, color: '#FAC33D' }}>46878人</Text>购买
+              </Text>
+            </View>
+
+            <View
+              style={{
+                paddingHorizontal: 24,
+                gap: 6,
+              }}>
+              <TouchableOpacity
+                style={styles.purchaseButton}
+                onPress={onPurchase}
+              >
+                <Text style={styles.purchaseButtonText}>
+                  继续抢购
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.cancelButton}
+                onPress={onClose}
+              >
+                <Text style={styles.cancelButtonText}>
+                  放弃机会
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </LinearGradient>
+        </View >
+      </View >
+    );
+}
+
+const styles = StyleSheet.create({
+  purchaseButton: {
+    backgroundColor: "#FAC33D",
+    borderRadius: 8,
+    paddingHorizontal: 20,
+    paddingVertical: 6,
+    alignItems: "center",
+  },
+  purchaseButtonText: {
+    color: "#1D2023",
+    fontSize: 14,
+    fontWeight: "500",
+    lineHeight: 25,
+    fontFamily: "PingFang SC",
+  },
+  cancelButton: {
+    backgroundColor: "#121314",
+    borderRadius: 8,
+    paddingHorizontal: 20,
+    paddingVertical: 6,
+    alignItems: "center",
+  },
+  cancelButtonText: {
+    color: "white",
+    fontSize: 13,
+    fontWeight: "500",
+    lineHeight: 25,
+    textAlign: 'center',
+    fontFamily: "PingFang SC",
+  },
+  titleText: {
+    textAlign: 'center',
+    color: '#F4DBBA',
+    fontSize: 17,
+    fontFamily: 'PingFang SC',
+    fontWeight: '600',
+    lineHeight: 22,
+    letterSpacing: 1,
+  },
+  countdownText: {
+    textAlign: "center",
+    color: 'white',
+    fontSize: 14,
+    fontFamily: 'Anton-Regular',
+    fontWeight: '400',
+    lineHeight: 21,
+    letterSpacing: 1,
+  },
+  contentText1: {
+    fontFamily: 'PingFang SC',
+    fontSize: 14,
+    fontWeight: '400',
+    lineHeight: 22,
+    letterSpacing: 0.5,
+    textAlign: 'center',
+    color: 'white',
+  },
+  contentText2: {
+    fontFamily: 'PingFang SC',
+    fontSize: 14,
+    fontWeight: '500',
+    lineHeight: 22,
+    letterSpacing: 0.5,
+    textAlign: 'center',
+    color: '#FFEFDA',
+  },
+  image1: {
+    position: "absolute",
+    bottom: -30,
+    width: 148,
+    height: 156,
+  },
+  image2: {
+    position: "absolute",
+    top: 0,
+    width: '100%',
+    aspectRatio: 1208 / 496,
+  },
+  countdownContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 5,
+  },
+});
