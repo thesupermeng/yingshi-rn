53ca223e6b907444d6106f1898cce6ceff9dbace
supermeng <supermeng@redfin.tech>
2024-02-06 00:12:18 +0800

    enhance logout

--- src/screens/Profile/Configure.tsx
+++ src/screens/Profile/Configure.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import {
   View,
@@ -7,42 +7,49 @@ import {
   TextInput,
   TouchableOpacity,
   SafeAreaView,
-} from 'react-native';
-import ScreenContainer from '../../components/container/screenContainer';
-import { RootStackScreenProps } from '@type/navigationTypes';
-import { useFocusEffect, useNavigation, useTheme } from '@react-navigation/native';
+  ActivityIndicator,
+  Modal,
+} from "react-native";
+import ScreenContainer from "../../components/container/screenContainer";
+import { RootStackScreenProps } from "@type/navigationTypes";
+import {
+  useFocusEffect,
+  useNavigation,
+  useTheme,
+} from "@react-navigation/native";
 
-import TitleWithBackButtonHeader from '../../components/header/titleWithBackButtonHeader';
-import { Button, Dialog } from '@rneui/themed';
-import ShowMoreButton from '../../components/button/showMoreButton';
-import NotificationModal from '../../components/modal/notificationModal';
-import MoreArrow from '@static/images/more_arrow.svg';
-import ConfirmationModal from '../../components/modal/confirmationModal';
-import { useAppDispatch, useAppSelector } from '@hooks/hooks';
-import { clearStorageMemory } from '@redux/actions/settingsActions';
-import NetInfo, { NetInfoState } from '@react-native-community/netinfo';
+import TitleWithBackButtonHeader from "../../components/header/titleWithBackButtonHeader";
+import { Button, Dialog } from "@rneui/themed";
+import ShowMoreButton from "../../components/button/showMoreButton";
+import NotificationModal from "../../components/modal/notificationModal";
+import MoreArrow from "@static/images/more_arrow.svg";
+import ConfirmationModal from "../../components/modal/confirmationModal";
+import { useAppDispatch, useAppSelector } from "@hooks/hooks";
+import { clearStorageMemory } from "@redux/actions/settingsActions";
+import NetInfo, { NetInfoState } from "@react-native-community/netinfo";
 
-import { removeUserAuthState } from '@redux/actions/userAction';
-import { changeScreenAction } from '@redux/actions/screenAction';
-import { RootState } from '@redux/store';
-import { userModel } from '@type/userType';
+import { removeUserAuthState } from "@redux/actions/userAction";
+import { changeScreenAction } from "@redux/actions/screenAction";
+import { RootState } from "@redux/store";
+import { userModel } from "@type/userType";
 
-import { APP_VERSION } from '@utility/constants';
-import { SettingsReducerState } from '@redux/reducers/settingsReducer';
-import { GoogleSignin } from '@react-native-google-signin/google-signin';
-import { CApi } from '@utility/apiService';
-import { clearMinivodApiCache } from "../../utils/minivodDownloader"
-import { UserApi } from '../../api/user';
+import { APP_VERSION } from "@utility/constants";
+import { SettingsReducerState } from "@redux/reducers/settingsReducer";
+import { GoogleSignin } from "@react-native-google-signin/google-signin";
+import { CApi } from "@utility/apiService";
+import { clearMinivodApiCache } from "../../utils/minivodDownloader";
+import { UserApi } from "../../api/user";
 import { addUserAuthState } from "@redux/actions/userAction";
 
-export default ({ navigation }: RootStackScreenProps<'设置'>) => {
+export default ({ navigation }: RootStackScreenProps<"设置">) => {
   const { colors, textVariants, icons, spacing } = useTheme();
   const [isVersionDialogOpen, setIsVersionDialogOpen] = useState(false);
   const [isClearDialogOpen, setIsClearDialogOpen] = useState(false);
 
   const [isLogoutDialogOpen, setIsLogoutDialogOpen] = useState(false);
 
-  const [subtitle1, setSubtitle1] = useState('当前已是最新版本' + APP_VERSION);
+  const [subtitle1, setSubtitle1] = useState("当前已是最新版本" + APP_VERSION);
+  const [isBackdropVisible, setIsBackdropVisible] = useState(false); // New state for backdrop visibility
 
   const settingsReducer: SettingsReducerState = useAppSelector(
     ({ settingsReducer }: RootState) => settingsReducer
@@ -67,15 +74,17 @@ export default ({ navigation }: RootStackScreenProps<'设置'>) => {
     setIsOffline(settingsReducer.isOffline);
   }, []);
 
-  useFocusEffect(useCallback(() => {
-    if (!settingsReducer.isOffline) {
-      setIsOffline(settingsReducer.isOffline);
-    } else {
-      return () => {
+  useFocusEffect(
+    useCallback(() => {
+      if (!settingsReducer.isOffline) {
         setIsOffline(settingsReducer.isOffline);
+      } else {
+        return () => {
+          setIsOffline(settingsReducer.isOffline);
+        };
       }
-    }
-  }, [settingsReducer.isOffline]));
+    }, [settingsReducer.isOffline])
+  );
 
   const guestLoginInit = async () => {
     // console.log("guestLoginInit");
@@ -95,7 +104,8 @@ export default ({ navigation }: RootStackScreenProps<'设置'>) => {
       userName: resultData.user.user_name,
       userReferralCode: resultData.user.user_referral_code,
       userEmail: resultData.user.user_email,
-      userPhoneNumber: resultData.user.user_phone !== 0 ? resultData.user.user_phone : '',
+      userPhoneNumber:
+        resultData.user.user_phone !== 0 ? resultData.user.user_phone : "",
       userMemberExpired: resultData.user.vip_end_time,
       // userMemberExpired: resultData.user.created_at,
       userReferrerName: resultData.user.referrer_name,
@@ -114,19 +124,19 @@ export default ({ navigation }: RootStackScreenProps<'设置'>) => {
     // console.log(json);
 
     await dispatch(addUserAuthState(json));
-
   };
-
 
   // useEffect(() => {
   //   dispatch(changeScreenAction('showSuccessLogin'));
   // }, []);
   const userState: userModel = useAppSelector(
-    ({ userReducer }: RootState) => userReducer,
+    ({ userReducer }: RootState) => userReducer
   );
   return (
     <ScreenContainer>
-      <View style={{ gap: spacing.m, justifyContent: 'space-between', flex: 1 }}>
+      <View
+        style={{ gap: spacing.m, justifyContent: "space-between", flex: 1 }}
+      >
         <View>
           <TitleWithBackButtonHeader title="设置" />
 
@@ -159,23 +169,20 @@ export default ({ navigation }: RootStackScreenProps<'设置'>) => {
           <ConfirmationModal
             onConfirm={async () => {
               //    user logout
+
+              setIsBackdropVisible(true); //
+              toggleLogoutDialog();
               await AsyncStorage.removeItem("showAds");
               await dispatch(removeUserAuthState());
-              clearMinivodApiCache()
-
+              clearMinivodApiCache();
 
               await guestLoginInit();
-
-              navigator.navigate('Home', {
-                screen: 'Profile',
+              setIsBackdropVisible(false); //
+              navigator.navigate("Home", {
+                screen: "Profile",
               });
-              toggleLogoutDialog();
 
               GoogleSignin.signOut();
-
-
-
-
             }}
             onCancel={toggleLogoutDialog}
             isVisible={isLogoutDialogOpen}
@@ -198,7 +205,8 @@ export default ({ navigation }: RootStackScreenProps<'设置'>) => {
                         ...textVariants.small,
                         paddingBottom: 3,
                         color: colors.muted,
-                      }}>
+                      }}
+                    >
                       当前版本{APP_VERSION}
                     </Text>
                     <MoreArrow
@@ -212,24 +220,38 @@ export default ({ navigation }: RootStackScreenProps<'设置'>) => {
             </View>
           </View>
         </View>
-        {userState.userToken != '' && (userState.userEmail != '' || userState.userPhoneNumber != '') && (
-          <TouchableOpacity onPress={toggleLogoutDialog}>
-            <View
-              style={{
-                backgroundColor: '#1d2023',
-                width: '100%',
-                height: 50,
-                borderRadius: 8,
-                borderWidth: 0,
-                justifyContent: 'center',
-                alignItems: 'center',
-                marginBottom: 30,
-              }}>
-              <Text style={{ color: '#FF3C3C' }}>退出登录</Text>
-            </View>
-          </TouchableOpacity>
-        )}
+        {userState.userToken != "" &&
+          (userState.userEmail != "" || userState.userPhoneNumber != "") && (
+            <TouchableOpacity onPress={toggleLogoutDialog}>
+              <View
+                style={{
+                  backgroundColor: "#1d2023",
+                  width: "100%",
+                  height: 50,
+                  borderRadius: 8,
+                  borderWidth: 0,
+                  justifyContent: "center",
+                  alignItems: "center",
+                  marginBottom: 30,
+                }}
+              >
+                <Text style={{ color: "#FF3C3C" }}>退出登录</Text>
+              </View>
+            </TouchableOpacity>
+          )}
       </View>
+
+      {/* Loading spinner with backdrop */}
+      <Modal
+        transparent
+        animationType="slide"
+        visible={isBackdropVisible}
+        onRequestClose={() => setIsBackdropVisible(false)}
+      >
+        <View style={styles.modalContainer}>
+          <ActivityIndicator size="large" color="#FAC33D" />
+        </View>
+      </Modal>
     </ScreenContainer>
   );
 };
@@ -239,31 +261,37 @@ const styles = StyleSheet.create({
     paddingTop: 10,
     paddingLeft: 10,
     paddingRight: 10,
-    display: 'flex',
-    flexDirection: 'row',
-    width: '100%',
-    alignItems: 'center',
-    justifyContent: 'space-between',
+    display: "flex",
+    flexDirection: "row",
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "space-between",
   },
   logo: {
-    flexDirection: 'row',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "row",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
   },
   submitBtn: {
     borderRadius: 10,
   },
   icon: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
   },
   overlay: {
     borderRadius: 14,
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
     padding: 20,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
   },
 });
