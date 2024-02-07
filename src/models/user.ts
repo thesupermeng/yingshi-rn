1db0ae608b39fa66ea41cdb24bfcc6168c7ea7b3
kelvin <kelvin@redfin.tech>
2024-02-07 12:12:12 +0800

    add default value to static user helper

--- src/models/user.ts
+++ src/models/user.ts
@@ -89,15 +89,21 @@ export class User {
         return this.userCurrentTimestamp < this.userMemberExpired;
     }
 
-    public static isGuest = (user: User): boolean => {
+    public static isGuest = (user?: User): boolean => {
+        if (user === null || user === undefined) return true;
+
         return user.userEmail === '' && user.userPhoneNumber === '';
     }
 
-    public static isLogin = (user: User): boolean => {
+    public static isLogin = (user?: User): boolean => {
+        if (user === null || user === undefined) return false;
+
         return user.userEmail !== '' || user.userPhoneNumber !== '';
     }
 
-    public static isVip = (user: User): boolean => {
+    public static isVip = (user?: User): boolean => {
+        if (user === null || user === undefined) return false;
+
         return user.userCurrentTimestamp < user.userMemberExpired;
     }
 }
