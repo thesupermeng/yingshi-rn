8be7c65636a070932519d34b08e3f57ed22643d1
kelvin <kelvin@redfin.tech>
2024-02-22 09:44:54 +0800

    bug fix -> when url change will reset play time

--- src/screens/Common/Play.tsx
+++ src/screens/Common/Play.tsx
@@ -615,7 +615,7 @@ const Play = ({ navigation, route }: RootStackScreenProps<"播放">) => {
       vod.vod_play_list = vodDetails.vod_play_list;
       vod.vod_play_url = vodDetails.vod_play_url;
       // setVod(vod);
-      dispatch(playVod(vod, currentTimeRef.current, currentEpisode, currentSourceId));
+      dispatch(playVod(vod, undefined, currentEpisode, currentSourceId));
     }
 
     const isRestricted = vodDetails?.vod_restricted === 1;
