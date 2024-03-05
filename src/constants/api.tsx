3215206e7229fdecd309be9ab7d6b640878b4874
supermeng <supermeng@redfin.tech>
2024-03-05 13:13:38 +0800

    首页加短剧

--- src/constants/api.tsx
+++ src/constants/api.tsx
@@ -10,7 +10,7 @@ export class CEndpoint {
     static userGetCountries = 'country/v1/country';
 
     static homeGetNav = 'nav/v1/navItems';
-    static homeGetPages = 'page/v3/typepage';
+    static homeGetPages = 'page/v3/typepage?dj=true';
 
     static playlistGetTopic = 'topic/v1/topic';
     static playlistGetTopicDetail = 'topic/v1/topic/detail';
