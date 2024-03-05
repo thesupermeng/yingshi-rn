72d01ff684bf4f88b135c1e9c500c313cc17a1b6
kelvin <kelvin@redfin.tech>
2024-03-05 10:35:42 +0800

    change umeng key

--- buildScripts/replaceChannel.js
+++ buildScripts/replaceChannel.js
@@ -15,7 +15,7 @@ let umengKey = '';
 // codePushDeploymentKey = "TJMfhAG-GnqKD6ygv0iOCXPvwwraf7MT-89Tm"
 // yingshi Production Key
 codePushDeploymentKey = 'yKccuj7iqcdUJBfjKi4dYg0Q4apstPlWZ2RxX';
-umengKey = '650938dcb2f6fa00ba556645';
+umengKey = '65e675eda7208a5af1b4eebb';
 
 // fs.readFile(constantsFilePath, 'utf8', (err, data) => {
 //     if (err) {
@@ -76,7 +76,7 @@ const replaceToChannelAndroid = {
 const replaceToUmengAppKey = {
   files: ['../android/app/build.gradle'],
   from: /resValue "string", "UMENG_APPKEY", "([^"]+)"/g,
-  to: 'resValue "string", "UMENG_APPKEY", "650938dcb2f6fa00ba556645"',
+  to: 'resValue "string", "UMENG_APPKEY", "65e675eda7208a5af1b4eebb"',
 };
 
 const replaceAppToponConf = {
