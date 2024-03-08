1131351cfe006c34769e889a8bc624100ef62673
kelvin <kelvin@redfin.tech>
2024-03-08 10:24:06 +0800

    bug fix -> no return 相关视频

--- src/screens/Common/Play.tsx
+++ src/screens/Common/Play.tsx
@@ -732,7 +732,7 @@ const Play = ({ navigation, route }: RootStackScreenProps<"播放">) => {
   }, [vodDetails]);
 
   const fetchVod = () => {
-    VodApi.getList({
+    return VodApi.getList({
       category: vod?.vod_class?.split(",").shift(),
       tid: vod?.type_id.toString() ?? "",
       limit: 6,
@@ -1557,7 +1557,7 @@ const Play = ({ navigation, route }: RootStackScreenProps<"播放">) => {
                       }}
                     >
                       <View
-                       onLayout={() => getPosition()}
+                        onLayout={() => getPosition()}
                         ref={componentRef}
                         style={{
                           display: "flex",
