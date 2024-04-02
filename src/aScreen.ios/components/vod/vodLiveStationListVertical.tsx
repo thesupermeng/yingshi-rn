9107b0ec0c49d2366d28523e8eda7895421a8133
supermeng <supermeng@redfin.tech>
2024-04-02 10:53:32 +0800

    fix url invite

--- src/aScreen.ios/components/vod/vodLiveStationListVertical.tsx
+++ src/aScreen.ios/components/vod/vodLiveStationListVertical.tsx
@@ -85,7 +85,7 @@ export default function VodLiveStationListVertical({
               <FastImage
                 style={{ flex: 1, borderRadius: 10 }}
                 source={{
-                  uri: item.live_station_img_url.charAt(0) == '/' ? 'https://dasdui.store' + item.live_station_img_url : item.live_station_img_url,
+                  uri: item.live_station_img_url.charAt(0) == '/' ? 'https://lebobotv.store' + item.live_station_img_url : item.live_station_img_url,
                   priority: 'normal',
                 }}
               />
