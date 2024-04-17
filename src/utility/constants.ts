69ee7b45de5cf72d5cff4914c0df16ca59099623
supermeng <supermeng@redfin.tech>
2024-04-17 17:09:48 +0800

    update const

--- src/utility/constants.ts
+++ src/utility/constants.ts
@@ -62,7 +62,7 @@ export const INVITE_DOMAIN = 'https://yingshi.tv/invite.html?invite=';
 //     'MI_GETAPPS': 'MI_GETAPPS',
 // }
 
-export const UMENG_CHANNEL = "GOOGLE_PLAY";
+export const UMENG_CHANNEL = "WEB";
 
 export const APP_VERSION = "1.7.8";
 
