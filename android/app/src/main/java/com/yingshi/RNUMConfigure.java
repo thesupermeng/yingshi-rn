5512dd654bf508fadfe5b2dc6abd24f5c31e731b
kelvin <kelvin@redfin.tech>
2024-04-25 10:44:09 +0800

    task done -> remove umeng

--- android/app/src/main/java/com/yingshi/RNUMConfigure.java
+++ android/app/src/main/java/com/yingshi/RNUMConfigure.java
@@ -1,47 +1,47 @@
-package com.yingshitv;
+// package com.yingshitv;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
+// import java.lang.reflect.InvocationTargetException;
+// import java.lang.reflect.Method;
 
-import android.annotation.TargetApi;
-import android.content.Context;
-import android.os.Build.VERSION_CODES;
-import android.util.Log;
+// import android.annotation.TargetApi;
+// import android.content.Context;
+// import android.os.Build.VERSION_CODES;
+// import android.util.Log;
 
-import com.umeng.commonsdk.UMConfigure;
+// import com.umeng.commonsdk.UMConfigure;
 
-import com.yingshitv.AnalyticsModule;
+// import com.yingshitv.AnalyticsModule;
 /**
  * Created by wangfei on 17/9/14.
  */
 
-public class RNUMConfigure {
+// public class RNUMConfigure {
 
-    public static void setLogEnabled(boolean isEnabled){
-        AnalyticsModule.showLog = isEnabled;
-        UMConfigure.setLogEnabled(isEnabled);
-    }
-    public static void preInit(Context context, String appkey, String channel){
-        UMConfigure.preInit(context,appkey,channel);
-    }
-    public static void init(Context context, String appkey, String channel, int type, String secret){
-        try {
-            initRN("react-native","2.0");
-            UMConfigure.init(context,appkey,channel,type,secret);
-        } catch (Exception e) {
-            Log.w("UMENG", e.getMessage());
-        }
-    }
-    @TargetApi(VERSION_CODES.KITKAT)
-    private static void initRN(String v, String t){
-        Method method = null;
-        try {
-            Class<?> config = Class.forName("com.umeng.commonsdk.UMConfigure");
-            method = config.getDeclaredMethod("setWraperType", String.class, String.class);
-            method.setAccessible(true);
-            method.invoke(null, v,t);
-        } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
-}
+//     public static void setLogEnabled(boolean isEnabled){
+//         // AnalyticsModule.showLog = isEnabled;
+//         UMConfigure.setLogEnabled(isEnabled);
+//     }
+//     public static void preInit(Context context, String appkey, String channel){
+//         UMConfigure.preInit(context,appkey,channel);
+//     }
+//     public static void init(Context context, String appkey, String channel, int type, String secret){
+//         try {
+//             initRN("react-native","2.0");
+//             UMConfigure.init(context,appkey,channel,type,secret);
+//         } catch (Exception e) {
+//             Log.w("UMENG", e.getMessage());
+//         }
+//     }
+//     @TargetApi(VERSION_CODES.KITKAT)
+//     private static void initRN(String v, String t){
+//         Method method = null;
+//         try {
+//             Class<?> config = Class.forName("com.umeng.commonsdk.UMConfigure");
+//             method = config.getDeclaredMethod("setWraperType", String.class, String.class);
+//             method.setAccessible(true);
+//             method.invoke(null, v,t);
+//         } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException | ClassNotFoundException e) {
+//             e.printStackTrace();
+//         }
+//     }
+// }
