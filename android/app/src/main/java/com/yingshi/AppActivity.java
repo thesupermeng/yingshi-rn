3726e8781cfa861c7d93ba8952b476eb3279d4d0
kelvin <kelvin@redfin.tech>
2024-06-13 17:14:36 +0800

    bug fix -> correct appid and appkey of topon

--- android/app/src/main/java/com/yingshi/AppActivity.java
+++ android/app/src/main/java/com/yingshi/AppActivity.java
@@ -50,7 +50,7 @@ public class AppActivity extends ReactActivity {
   protected void onCreate(Bundle savedInstanceState) {
     // super.onCreate(savedInstanceState); // or super.onCreate(null) with react-native-screens
     super.onCreate(null);
-    ATSDK.init(this, "a65096b9d21e23", "ad83e042ddc3e6904c1f63bcc575a6bd7");
+    ATSDK.init(this, "a65002a2db3a7f", "aad683307ed540dfc11b094206e8a15b1");
 
     // Create a ConsentRequestParameters object.
     ConsentRequestParameters params = new ConsentRequestParameters
