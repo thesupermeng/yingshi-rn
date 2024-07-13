ea0e5e862d7667d56a1fe2191d4c157610f7b082
tongduan <tongduan@126.com>
2024-07-14 00:28:32 +0800

    release version 1.8.3  version code 81

--- src/utility/constants.ts
+++ src/utility/constants.ts
@@ -65,7 +65,7 @@ export const INVITE_DOMAIN = 'https://yingshi.tv/invite/pro/?invite=';
 
 export const UMENG_CHANNEL = "WEB";
 
-export const APP_VERSION = "1.8.2";
+export const APP_VERSION = "1.8.3";
 
 export const TOPON_ANDROID_APP_ID = 'a65002a2db3a7f';
 export const TOPON_ANDROID_APP_KEY = 'aad683307ed540dfc11b094206e8a15b1';
